1acab78c6c06581a84233f684773b8b50fa147d4
Evan Patterson <evan@epatters.org>
2024-04-24 21:21:08 -0700

    ENH: Require that `FinSet`s implement `IntoIterator` trait.

--- catlog/src/lib.rs
+++ catlog/src/lib.rs
@@ -1,20 +1,38 @@
+use std::ops::Range;
 use std::hash::Hash;
 use std::collections::HashSet;
 
+/** An arbitrary set.
+
+The interface is minimal. A set has an element type (`Elem`) and can check
+whether a value of that type belongs to the set.
+ */
 pub trait Set {
     type Elem;
 
+    /// Does the set contain the element `x`?
     fn contains(&self, x: &Self::Elem) -> bool;
 }
 
-pub trait FinSet: Set {
+/** A finite set.
+
+Finite sets know their size and are iterable.
+ */
+pub trait FinSet: Set + IntoIterator {
+    /// Returns the size of the finite set.
     fn len(&self) -> usize;
 
+    /// Is the set empty?
     fn is_empty(&self) -> bool {
         self.len() == 0
     }
 }
 
+/** A skeletal finite set.
+
+The elements of the skeletal finite set of size `n` are the numbers `0..n`
+(excluding `n`).
+ */
 #[derive(Clone,Copy)]
 pub struct FinSetSkel(usize);
 
@@ -26,10 +44,20 @@ impl Set for FinSetSkel {
     }
 }
 
+impl IntoIterator for FinSetSkel {
+    type Item = usize;
+    type IntoIter = Range<usize>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        0..(self.0)
+    }
+}
+
 impl FinSet for FinSetSkel {
     fn len(&self) -> usize { self.0 }
 }
 
+/// A finite set backed by a hash set.
 pub struct FinSetHash<T>(HashSet<T>);
 
 impl<T: Eq + Hash> Set for FinSetHash<T> {
@@ -38,6 +66,12 @@ impl<T: Eq + Hash> Set for FinSetHash<T> {
     fn contains(&self, x: &T) -> bool { self.0.contains(x) }
 }
 
+impl<T: Eq + Hash> IntoIterator for FinSetHash<T> {
+    type Item = T;
+    type IntoIter = std::collections::hash_set::IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter { self.0.into_iter() }
+}
 impl<T: Eq + Hash> FinSet for FinSetHash<T> {
     fn len(&self) -> usize { self.0.len() }
     fn is_empty(&self) -> bool { self.0.is_empty() }
@@ -48,11 +82,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn fin_set_int() {
+    fn fin_set_skel() {
         let s = FinSetSkel(3);
         assert!(!s.is_empty());
         assert_eq!(s.len(), 3);
         assert!(s.contains(&2));
         assert!(!s.contains(&3));
+        let elems: Vec<usize> = s.into_iter().collect();
+        assert_eq!(elems, vec![0,1,2]);
     }
 }
