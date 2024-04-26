104c90c6d25460de67787007f62c08d00882c7de
Owen Lynch <root@owenlynch.org>
2024-04-25 18:50:35 -0700

    it's prettier now

--- relation-macro-editor/src/App.tsx
+++ relation-macro-editor/src/App.tsx
@@ -1,6 +1,131 @@
 import * as Y from 'yjs'
 import { WebsocketProvider } from 'y-websocket'
 import { createSignal, For } from 'solid-js'
+import './App.css'
+
+function PortRow(port: Y.Map<any>, deleteMe: () => void, junctions: () => Array<Y.Map<any>>) {
+  let [name, setName] = createSignal<string>('')
+  let [junction, setJunction] = createSignal<string>('')
+  setName(port.get('name'))
+  setJunction(port.get('junction'))
+  port.observe(_ => {
+    setName(port.get('name'))
+    setJunction(port.get('junction'))
+  })
+  return (<tr>
+    <td>
+      <input type="text" value={name()} oninput={evt => port.set('name', evt.target.value)}></input>
+    </td>
+    <td>
+      <select value={junction()} oninput={evt => port.set('junction', evt.target.value)}>
+        <For each={junctions()}>
+          {junction => {
+            let [name, setName] = createSignal<string>('')
+            junction.observe(_ => {
+              setName(junction.get('name'))
+            })
+            setName(junction.get('name'))
+            return (<option value={name()}>{name()}</option>)
+          }}
+        </For>
+      </select>
+    </td>
+    <td class="trash">
+      <button class="delete-port" onclick={_ => deleteMe()}><i class="fa-solid fa-trash"></i></button>
+    </td>
+  </tr>)
+}
+
+function PortTable(box: Y.Map<any>, junctions: () => Array<Y.Map<any>>) {
+  let [ports, setPorts] = createSignal<Array<Y.Map<any>>>([])
+  let yports: Y.Array<Y.Map<any>> = box.get('ports')
+  setPorts(yports.toArray())
+  yports.observe(_ => {
+    setPorts(yports.toArray())
+  })
+  function newPort() {
+    let p = new Y.Map<any>()
+    p.set('name', '')
+    let js = junctions()
+    if (js.length >= 0) {
+      p.set('junction', js[0].get('name'))
+    } else {
+      p.set('junction', null)
+    }
+    yports.push([p])
+  }
+  return (<div>
+    <table class="port-table">
+      <thead>
+        <tr><th>Port name</th><th>Junction</th></tr>
+      </thead>
+      <tbody>
+        <For each={ports()}>{
+          (port, i) => PortRow(port, () => yports.delete(i()),junctions)
+        }</For>
+        <tr>
+          <td class="plus">
+            <button onclick={_ => newPort()}><i class="fa-solid fa-plus"></i></button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>)
+}
+
+function BoxEditor(box: Y.Map<any>, deleteMe: () => void, junctions: () => Array<Y.Map<any>>) {
+  let [name, setName] = createSignal<string>('')
+  setName(box.get('name'))
+  box.observe(_ => {
+    setName(box.get('name'))
+  })
+  return (
+    <li>
+      <div class="box-editor">
+        <div class="box-content">
+          <span><strong>Name: </strong></span>
+          <input type="text" value={name()} oninput={evt => box.set('name', evt.target.value)}></input>
+          {PortTable(box, junctions)}
+        </div>
+        <button onclick={_ => deleteMe()}><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </li>
+  )
+}
+
+function JunctionTable(junctions: () => Array<Y.Map<any>>, newJunction: () => void, deleteJunction: (i: number) => void) {
+  return (<table class="junctions-table">
+    <thead>
+      <tr class="junctions-header"><th>Name</th><th>Exposed</th></tr>
+    </thead>
+    <tbody>
+      <For each={junctions()}>
+        {(item, i) => {
+          let [text, setText] = createSignal<string>('')
+          setText(item.get('name'))
+          item.observe(_ => setText(item.get('name')))
+          return (<tr>
+            <td>
+              <input type="text" value={text()} oninput={evt => item.set('name', evt.target.value)}></input>
+            </td>
+            <td>
+              <input type="checkbox"></input>
+            </td>
+            <td class="trash">
+              <button onclick={_ => deleteJunction(i())}><i class="fa-solid fa-trash"></i></button>
+            </td>
+          </tr>)
+        }
+        }
+      </For>
+      <tr>
+        <td class="plus">
+          <button onclick={_ => newJunction()}><i class="fa-solid fa-plus"></i></button>
+        </td>
+      </tr>
+    </tbody>
+  </table>)
+}
 
 function App() {
   const ydoc = new Y.Doc()
@@ -37,6 +162,10 @@ function App() {
     yjunctions.push([j])
   }
 
+  function deleteJunction(i: number) {
+    yjunctions.delete(i)
+  }
+
   function newBox() {
     let b = new Y.Map<any>()
     let ports = new Y.Array<Y.Map<any>>()
@@ -47,94 +176,16 @@ function App() {
 
   return (
     <>
-      <div>Hello World</div>
       <h3>Junctions</h3>
-      <button onclick={_ => newJunction()}>Add Junction</button>
-      <button onclick={_ => yjunctions.delete(yjunctions.length-1)}>Delete Junction</button>
-      <ul>
-        <For each={junctions()}>
-          {item => {
-            let [text, setText] = createSignal<string>('')
-            setText(item.get('name'))
-            item.observe(_ => setText(item.get('name')))
-            return (<li>
-              <input type="text" value={text()} oninput={evt => item.set('name', evt.target.value)}></input>
-            </li>)
-          }
-          }
-        </For>
-      </ul>
+      {JunctionTable(junctions, newJunction, deleteJunction)}
       <h3>Boxes</h3>
-      <button onclick={_ => newBox()}>Add Box</button>
-      <button onclick={_ => yboxes.delete(yboxes.length-1)}>Delete Box</button>
-      <button onclick={_ => yboxes.delete(0, yboxes.length)}>Delete all boxes</button>
-      <ul>
+      <ul class="boxes">
         <For each={boxes()}>
-          {box => {
-            let [name, setName] = createSignal<string>('')
-            let [ports, setPorts] = createSignal<Array<Y.Map<any>>>([])
-            let yports: Y.Array<Y.Map<any>> = box.get('ports')
-            setName(box.get('name'))
-            setPorts(yports.toArray())
-            box.observe(_ => {
-              setName(box.get('name'))
-            })
-            yports.observe(_ => {
-              setPorts(yports.toArray())
-            })
-            function newPort() {
-              let p = new Y.Map<any>()
-              p.set('name', '')
-              let js = yjunctions.toArray()
-              if (js.length >= 1) {
-                p.set('junction', js[0].get('name'))
-              } else {
-                p.set('junction', null)
-              }
-              yports.push([p])
-            }
-            function removePort() {
-              yports.delete(yports.length - 1)
-            }
-            return (
-              <li>
-                <div class="boxeditor">
-                  <input type="text" value={name()} oninput={evt => box.set('name', evt.target.value)}></input>
-                  <button onclick={_ => newPort()}>Add Port</button>
-                  <button onclick={_ => removePort()}>Remove Port</button>
-                  <ul>
-                    <For each={ports()}>
-                      {port => {
-                        let [name, setName] = createSignal<string>('')
-                        let [junction, setJunction] = createSignal<string>('')
-                        setName(port.get('name'))
-                        setJunction(port.get('junction'))
-                        port.observe(_ => {
-                          setName(port.get('name'))
-                          setJunction(port.get('junction'))
-                        })
-                        return (<li>
-                          <input type="text" value={name()} oninput={evt => port.set('name', evt.target.value)}></input>
-                          <select value={junction()} oninput={evt => port.set('junction', evt.target.value)}>
-                            <For each={junctions()}>
-                              {junction => {
-                                let [name, setName] = createSignal<string>('')
-                                junction.observe(_ => {
-                                  setName(junction.get('name'))
-                                })
-                                setName(junction.get('name'))
-                                return (<option value={name()}>{name()}</option>)
-                              }}
-                            </For>
-                          </select>
-                        </li>)
-                      }}
-                    </For>
-                  </ul>
-                </div>
-              </li>
-            )
-          }}</For>
+          {(box, i) => BoxEditor(box, () => {yboxes.delete(i())}, junctions)}
+        </For>
+        <li class="addbox">
+          <button onclick={_ => newBox()}><i class="fa-solid fa-plus"></i></button>
+        </li>
       </ul>
     </>
   )
