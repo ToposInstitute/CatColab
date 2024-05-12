2de74727e3f7f7719ca79971965be9c2d9bb5dc9
Evan Patterson <evan@epatters.org>
2024-05-12 13:47:06 -0700

    DOC: Explain organization of top-level modules.

--- catlog/src/lib.rs
+++ catlog/src/lib.rs
@@ -1,4 +1,23 @@
-//! A toolbox for categorical logic based on double theories.
+/*! A toolbox for categorical logic based on double-categorical theories.
+
+# Organization
+
+While the purpose of this package is to implement double theories and models,
+having a certain amount of lower-dimensional category theory is inevitably
+useful as a foundation. For instance, the starting point for a double graph or a
+[double computad](crate::dbl::computad) is a pair of graphs that share the same
+set of vertices. The package is organized into top-level modules according to
+dimensionality:
+
+0. [`zero`]: Sets and mappings, known semi-seriously as zero-dimensional
+   category theory.
+1. [`one`]: Ordinary, or one-dimensional, category theory.
+2. [`dbl`]: Double category theory.
+
+The foundational modules make no pretence to completeness, but if they become
+sufficiently useful in their own right, they may be spun off into their own
+packages.
+*/
 
 #![allow(mixed_script_confusables)] // Unicode identifiers.
 #![warn(missing_docs)]
