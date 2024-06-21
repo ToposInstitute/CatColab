879c83582339140f54ebf35005a8b89bb7d01f5f
Evan Patterson <evan@epatters.org>
2024-06-21 15:01:44 -0700

    DOC: Overview of morphism types in a double theory.

--- catlog/src/dbl/theory.rs
+++ catlog/src/dbl/theory.rs
@@ -1,12 +1,12 @@
 /*! Double theories.
 
-A double theory specifies, via equational axioms, a categorical structure,
-namley a category (or categories) equipped with extra structure. The spirit of
-the formalism is that a double theory is "just" a double category, categorifying
-Lawvere's idea that a theory is "just" a category. Nevertheless, double theories
-come with intuitions more specific than those attached to an arbitrary double
-category. To bring these out, the interface for double theories, [`DblTheory`],
-introduces new terminology compared to the references cited below.
+A double theory equationally specifies a categorical structure: a category (or
+categories) equipped with extra structure. The spirit of the formalism is that a
+double theory is "just" a double category, categorifying Lawvere's idea that a
+theory is "just" a category. Nevertheless, double theories come with intuitions
+more specific than those attached to an arbitrary double category. To bring
+these out, the interface for double theories, [`DblTheory`], introduces new
+terminology compared to the references cited below.
 
 # Terminology
 
@@ -14,8 +14,8 @@ A double theory comprises four kinds of things:
 
 1. **Object type**, interpreted in models as a set of objects.
 
-2. **Morphism type**, having a source and atarget object type and interpreted in
-   models as a span of morphisms (or
+2. **Morphism type**, having a source and a target object type and interpreted
+   in models as a span of morphisms (or
    [heteromorphisms](https://ncatlab.org/nlab/show/heteromorphism)) between sets
    of objects.
 
@@ -28,20 +28,28 @@ A double theory comprises four kinds of things:
 The dictionary between the type-theoretic and double-categorical terminology is
 summarized by the table:
 
-| Double theory      | Double category           | Interpreted as |
-|--------------------|---------------------------|----------------|
-| Object type        | Object                    | Set            |
-| Morphism type      | Proarrow (loose morphism) | Span of sets   |
-| Object operation   | Arrow (tight morphism)    | Function       |
-| Morphism operation | Cell                      | Map of spans   |
+| Associated type                 | Double theory      | Double category           | Interpreted as |
+|---------------------------------|--------------------|---------------------------|----------------|
+| [`ObType`](DblTheory::ObType)   | Object type        | Object                    | Set            |
+| [`MorType`](DblTheory::MorType) | Morphism type      | Proarrow (loose morphism) | Span           |
+| [`ObOp`](DblTheory::ObOp)       | Object operation   | Arrow (tight morphism)    | Function       |
+| [`MorOp`](DblTheory::MorOp)     | Morphism operation | Cell                      | Map of spans   |
 
-Models of a double theory are always *categorical* structures, rather than
-merely *set-theoretical* ones, because each object type is interpreted not just
-as a set of objects but also as a set of morphisms between those objects,
-constituting a category. To achieve this, each object type in the double theory
-has a distinguished morphism or "hom" type ([`hom_type`](DblTheory::hom_type)).
-Similarly, all object operations become functorial since they are associated
-with a distinguished operation on morphisms ([`hom_op`](DblTheory::hom_op)).
+Models of a double theory are automatically *categorical* structures, rather
+than merely *set-theoretical* ones, because each object type is assigned not
+just a set of objects but also a span of morphisms between those objects,
+constituting a category. The morphism data comes from a distinguished "Hom" type
+for each object type in the double theory. Similarly, each object operation is
+automatically functorial since it comes with a "Hom" operation between the Hom
+types. Moreover, morphism types and operations can be composed to give new ones,
+as summarized by the table:
+
+| Method                                      | Double theory               | Double category        |
+|---------------------------------------------|-----------------------------|------------------------|
+| [`hom_type`](DblTheory::hom_type)           | Hom type                    | Identity proarrow      |
+| [`hom_op`](DblTheory::hom_op)               | Hom operation               | Identity cell on arrow |
+| [`compose_types`](DblTheory::compose_types) | Compose morphism types      | Compose proarrows      |
+| `compose_hom_ops`                           | Compose morphism operations | Compose cells          |
 
 # References
 
@@ -72,7 +80,7 @@ pub trait DblTheory {
 
     Viewing the theory as a double category, this is the type of proarrows.
     */
-    type HomType;
+    type MorType;
 
     /** Rust type of operations on objects in the double theory.
 
@@ -84,13 +92,13 @@ pub trait DblTheory {
 
     Viewing the theory as a double category, this is the type of cells.
     */
-    type HomOp;
+    type MorOp;
 
     /// Source of morphism type.
-    fn src(&self, m: &Self::HomType) -> Self::ObType;
+    fn src(&self, m: &Self::MorType) -> Self::ObType;
 
     /// Target of morphism type.
-    fn tgt(&self, m: &Self::HomType) -> Self::ObType;
+    fn tgt(&self, m: &Self::MorType) -> Self::ObType;
 
     /// Domain of operation on objects.
     fn dom(&self, f: &Self::ObOp) -> Self::ObType;
@@ -99,29 +107,29 @@ pub trait DblTheory {
     fn cod(&self, f: &Self::ObOp) -> Self::ObType;
 
     /// Source operation of operation on morphisms.
-    fn op_src(&self, α: &Self::HomOp) -> Self::ObOp;
+    fn op_src(&self, α: &Self::MorOp) -> Self::ObOp;
 
     /// Target operation of operation on morphisms.
-    fn op_tgt(&self, α: &Self::HomOp) -> Self::ObOp;
+    fn op_tgt(&self, α: &Self::MorOp) -> Self::ObOp;
 
     /// Domain type of operation on morphisms.
-    fn op_dom(&self, α: &Self::HomOp) -> Self::HomType;
+    fn op_dom(&self, α: &Self::MorOp) -> Self::MorType;
 
     /// Codomain type of operation on morphisms.
-    fn op_cod(&self, α: &Self::HomOp) -> Self::HomType;
+    fn op_cod(&self, α: &Self::MorOp) -> Self::MorType;
 
     /// Composes a sequence of morphism types.
     fn compose_types(
         &self,
-        path: Path<Self::ObType, Self::HomType>
-    ) -> Self::HomType;
+        path: Path<Self::ObType, Self::MorType>
+    ) -> Self::MorType;
 
     /** Type of morphism associated with object type.
 
     Viewing the theory as a double category, this is the identity proarrow on an
     object.
     */
-    fn hom_type(&self, x: Self::ObType) -> Self::HomType {
+    fn hom_type(&self, x: Self::ObType) -> Self::MorType {
         self.compose_types(Path::Id(x))
     }
 
@@ -145,5 +153,5 @@ pub trait DblTheory {
     Viewing the theory as a double category, this is the identity cell on an
     arrow.
     */
-    fn hom_op(&self, f: Self::ObOp) -> Self::HomOp;
+    fn hom_op(&self, f: Self::ObOp) -> Self::MorOp;
 }
