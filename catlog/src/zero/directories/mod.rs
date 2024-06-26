95e0dc27c37faf927ab03c7eba64ccacba64fb23
Owen Lynch <root@owenlynch.org>
2024-06-26 12:03:18 -0700

    basic operations for directories

--- catlog/src/zero/directories/mod.rs
+++ catlog/src/zero/directories/mod.rs
@@ -1,24 +1,23 @@
-pub mod nonemptymap;
+//! Directories and associated operations
+
 pub mod paths;
 
-pub use nonemptymap::*;
 pub use paths::*;
 
 use archery::*;
-use std::collections::HashMap;
-use std::hash::Hash;
+use std::collections::BTreeMap;
 
 /// Non-empty directories
 #[derive(PartialEq, Eq)]
-enum NonEmptyDir<K: Hash + Eq + Clone, V, P: SharedPointerKind> {
+enum NonEmptyDir<K: Ord + Eq + Clone, V, P: SharedPointerKind> {
     Leaf(SharedPointer<V, P>),
-    Node(HashMap<K, SharedPointer<NonEmptyDir<K, V, P>, P>>),
+    Node(SharedPointer<BTreeMap<K, NonEmptyDir<K, V, P>>, P>),
 }
 use NonEmptyDir::*;
 
 impl<K, V, P> Clone for NonEmptyDir<K, V, P>
 where
-    K: Hash + Eq + Clone,
+    K: Ord + Eq + Clone,
     P: SharedPointerKind,
 {
     fn clone(&self) -> Self {
@@ -31,7 +30,7 @@ where
 
 impl<K, V, P> NonEmptyDir<K, V, P>
 where
-    K: Hash + Eq + Clone,
+    K: Ord + Eq + Clone,
     P: SharedPointerKind,
 {
     /// Produce a new non-empty directory with the same shape as self,
@@ -42,14 +41,13 @@ where
     {
         match self {
             Leaf(v) => Leaf(SharedPointer::new(f(v))),
-            Node(m) => Node(HashMap::from_iter(
-                m.iter()
-                    .map(|(k, d)| (k.clone(), SharedPointer::new(d.map(f)))),
-            )),
+            Node(m) => Node(SharedPointer::new(BTreeMap::from_iter(
+                m.iter().map(|(k, d)| (k.clone(), d.map(f))),
+            ))),
         }
     }
 
-    fn get(&self, p: &Path<K>) -> Option<&V> {
+    fn get(&self, p: &Path<K, P>) -> Option<&V> {
         match (self, p.uncons()) {
             (Leaf(v), None) => Some(v),
             (Node(m), Some((k, p))) => m.get(&k).and_then(|ned| ned.get(&p)),
@@ -57,69 +55,196 @@ where
         }
     }
 
-    fn insert(&mut self, p: &Path<K>, v: V) -> Option<()> {
+    fn insert_mut(&mut self, p: &Path<K, P>, v: V) -> Option<()> {
         match (self, p.uncons()) {
             (Leaf(old_v), None) => {
                 *old_v = SharedPointer::new(v);
                 Some(())
             }
-            (Node(m), Some((k, p))) => match m.get_mut(&k) {
-                None => {
-                    m.insert(k, SharedPointer::new(NonEmptyDir::singleton(&p, v)));
-                    Some(())
+            (Node(m), Some((k, p))) => {
+                let m = SharedPointer::make_mut(m);
+                match m.get_mut(&k) {
+                    None => {
+                        m.insert(k, NonEmptyDir::singleton(&p, v));
+                        Some(())
+                    }
+                    Some(ned) => ned.insert_mut(&p, v),
                 }
-                Some(ned) => SharedPointer::make_mut(ned).insert(&p, v),
-            },
+            }
             _ => None,
         }
     }
 
-    fn singleton(p: &Path<K>, v: V) -> Self {
+    fn singleton(p: &Path<K, P>, v: V) -> Self {
         match p.uncons() {
             None => Leaf(SharedPointer::new(v)),
             Some((k, p)) => {
-                let mut m = HashMap::new();
-                m.insert(k, SharedPointer::new(NonEmptyDir::singleton(&p, v)));
-                Node(m)
+                let mut m = BTreeMap::new();
+                m.insert(k, NonEmptyDir::singleton(&p, v));
+                Node(SharedPointer::new(m))
             }
         }
     }
 
-    fn try_from_iter<T: IntoIterator<Item = (K, SharedPointer<NonEmptyDir<K, V, P>, P>)>>(
+    fn try_from_iter<T: IntoIterator<Item = (K, NonEmptyDir<K, V, P>)>>(
         iter: T,
     ) -> Option<NonEmptyDir<K, V, P>> {
         let mut iter = iter.into_iter();
         match iter.next() {
             None => None,
             Some((k, m)) => {
-                let mut map = HashMap::new();
+                let mut map = BTreeMap::new();
                 map.insert(k, m);
                 map.extend(iter);
-                Some(NonEmptyDir::Node(map))
+                Some(NonEmptyDir::Node(SharedPointer::new(map)))
+            }
+        }
+    }
+
+    pub fn filter_flatmap<W, F: Fn(&V) -> Option<NonEmptyDir<K, W, P>>>(
+        &self,
+        f: F,
+    ) -> Option<NonEmptyDir<K, W, P>> {
+        match self {
+            Leaf(v) => f(v),
+            Node(m) => {
+                let m_next: BTreeMap<K, NonEmptyDir<K, W, P>> = m
+                    .iter()
+                    .filter_map(|(k, ned)| {
+                        ned.filter_flatmap(&f).map(|ned| (k.clone(), ned.clone()))
+                    })
+                    .collect();
+                if m_next.len() == 0 {
+                    None
+                } else {
+                    Some(Node(SharedPointer::new(m_next)))
+                }
             }
         }
     }
 }
 
-/// Possibly-empty directory
-pub struct Dir<K: Hash + Eq + Clone, V, P: SharedPointerKind>(Option<NonEmptyDir<K, V, P>>);
+impl<K, V, P> NonEmptyDir<K, NonEmptyDir<K, V, P>, P>
+where
+    K: Ord + Eq + Clone,
+    P: SharedPointerKind,
+{
+    fn flatten(&self) -> NonEmptyDir<K, V, P> {
+        match self {
+            Leaf(ned) => ned.as_ref().clone(),
+            Node(m) => Node(SharedPointer::new(
+                m.iter()
+                    .map(|(p, ned)| (p.clone(), ned.flatten()))
+                    .collect(),
+            )),
+        }
+    }
+}
+
+/// A directory is a special type of [trie][1] which only stores values at root nodes.
+///
+/// Conceptually, this can be thought of as a map from [`Path<K, P>`]s to values,
+/// such that the domain of the map is *prefix-free* (no path in the domain is a
+/// prefix of any other path).
+///
+/// One intended use for directories is to manage the namespace of variables in a
+/// scientific model. Directories allow models to be composed without their variable names
+/// clashing. In accordance with this intended use, directories store each node
+/// (which is a mapping from elements of `K` to subdirectories) in sorted order,
+/// so that iterating through directories happens in a predictable, deterministic order.
+/// Specifically, iterating through all paths in a directory happens in lexicographic order.
+///
+/// Mainly, this implementation is a tree with reference-counted pointers. This means that
+/// multiple directories may share subdirectories. Directories are also cheap to clone.
+/// However we take advantage of [`SharedPointer::make_mut`] in order
+/// to mutate in-place when the reference count of a pointer tells us that no other
+/// references to a given value are live. Thus, we also have mutating methods. Of course,
+/// these mutating methods can only be called when the caller has exclusive access.
+/// This strategy for blending mutation and persistent data structures is inspired by
+/// the [Perceus garbage collector][2].
+///
+/// [1]: https://en.wikipedia.org/wiki/Trie
+/// [2]: https://koka-lang.github.io/koka/doc/book.html#why-fbip
+pub struct Dir<K: Ord + Eq + Clone, V, P: SharedPointerKind>(Option<NonEmptyDir<K, V, P>>);
+
+impl<K, V, P> Clone for Dir<K, V, P>
+where
+    K: Ord + Eq + Clone,
+    P: SharedPointerKind,
+{
+    fn clone(&self) -> Self {
+        Dir(self.0.clone())
+    }
+}
 
 impl<K, V, P> Dir<K, V, P>
 where
-    K: Hash + Eq + Clone,
+    K: Ord + Eq + Clone,
     P: SharedPointerKind,
 {
-    /// Create a new directory with the same shape as self, but
-    /// whose values are mapped over by f
+    /// Returns the empty directory
+    pub fn empty() -> Self {
+        Dir(None)
+    }
+
+    /// Returns a new directory with the same domain as self, but
+    /// whose value at path `p` is given by applying `f` to the value at `p` in `self`.
     pub fn map<W, F>(&self, f: &F) -> Dir<K, W, P>
     where
         F: Fn(&V) -> W,
     {
-        match self {
-            Dir(Some(ned)) => Dir(Some(ned.map(f))),
+        match &self.0 {
+            Some(ned) => Dir(Some(ned.map(f))),
             _ => Dir(None),
         }
     }
 
-    pub fn get(&self, p: &Path) ->
+    /// Returns a reference to the value corresponding to the path.
+    pub fn get(&self, p: &Path<K, P>) -> Option<&V> {
+        match &self.0 {
+            None => None,
+            Some(ned) => ned.get(p),
+        }
+    }
+
+    /// Attempts to insert `v` at path `p` via in-place mutation. If `p`
+    /// is a prefix of any path that is currently in `self`, then this will return
+    /// `None` and not perform any mutation.
+    pub fn insert_mut(&mut self, p: &Path<K, P>, v: V) -> Option<()> {
+        match &mut self.0 {
+            None => {
+                *self = Dir(Some(NonEmptyDir::singleton(p, v)));
+                Some(())
+            }
+            Some(ned) => ned.insert_mut(p, v),
+        }
+    }
+
+    /// Attempts to create a copy of `self` with `v` inserted at path `p`.
+    /// If `p` is a prefix of any path that is currently in `self`, then
+    /// this will return `None`.
+    pub fn insert(&self, p: &Path<K, P>, v: V) -> Option<Self> {
+        let mut d: Self = self.clone();
+        d.insert_mut(p, v).map(|_| d)
+    }
+}
+
+impl<K, V, P> Dir<K, Dir<K, V, P>, P>
+where
+    K: Ord + Eq + Clone,
+    P: SharedPointerKind,
+{
+    /// Returns a new directory whose paths are given by `p1.concat(p2)` where
+    /// `p1` is in the domain of self, and `p2` is in the domain of `self[p1]`,
+    /// and whose value at `p1.concat(p2)` is `self[p1][p2]`.
+    ///
+    /// This is the fundamental namespacing operation for directories; if we have
+    /// a directory of models that we wish to compose, the directory of variables
+    /// in the composite model is given by flattening.
+    pub fn flatten(&self) -> Dir<K, V, P> {
+        match &self.0 {
+            None => Dir(None),
+            Some(ned) => Dir(ned.filter_flatmap(|d| d.0.clone())),
+        }
+    }
 }
--- catlog/src/zero/directories/paths.rs
+++ catlog/src/zero/directories/paths.rs
@@ -1,43 +1,71 @@
-use archery::{SharedPointer, SharedPointerKind};
-use smallvec::*;
+//! [`Path`] and associated operations
 
-/// A path in a K-indexed directory
-#[derive(Clone, PartialEq, Eq)]
+use archery::{SharedPointer, SharedPointerKind};
+
+/// A linked list with element type `K`. Used for indexing into directories.
+/// A path '/a/b/c' would be given by `Path::root().cons('c').cons('b').cons('a')`;
+/// the first element of the list is the first element used for indexing into
+/// a directory.
+///
+/// NOTE: This might conflict with paths for graphs?
+///
+/// NOTE: It might be more efficient to use a SmallVec instead of a linked list?
+#[derive(PartialEq, Eq)]
 pub struct Path<K: Clone, P: SharedPointerKind>(Option<(K, SharedPointer<Path<K, P>, P>)>);
+
+impl<K, P> Clone for Path<K, P>
+where
+    K: Clone,
+    P: SharedPointerKind,
+{
+    fn clone(&self) -> Self {
+        Path(self.0.clone())
+    }
+}
 
 impl<K, P> Path<K, P>
 where
     K: Clone,
     P: SharedPointerKind,
 {
+    /// Returns the root (empty) path, equivalent to '/' in unix
     pub fn root() -> Self {
         Path(None)
     }
 
+    /// Returns whether the path is the root
     pub fn isroot(&self) -> bool {
         self.0.is_none()
     }
 
+    /// Returns the length of the path
+    pub fn length(&self) -> usize {
+        match &self.0 {
+            None => 0,
+            Some((_, p)) => 1 + p.length(),
+        }
+    }
+
+    /// Destructs the path into the first element and rest of the path.
     pub fn uncons(&self) -> Option<(K, SharedPointer<Self, P>)> {
         self.0.clone()
     }
 
-    pub fn concat(&self, other: Path<K>) -> Self {
-        let mut new = self.0.clone();
-        new.extend(other.0.iter().map(|k| k.clone()));
-        Path(new)
+    /// Returns the concatenation of `self` and `other`. O(self.length())
+    pub fn concat(&self, other: &Path<K, P>) -> Self {
+        match &self.0 {
+            None => other.clone(),
+            Some((k, p)) => Path(Some((k.clone(), SharedPointer::new(p.concat(other))))),
+        }
     }
 
+    /// Adds `k` to the *front* of the list. O(1)
     pub fn cons(&self, k: K) -> Self {
-        let mut new = SmallVec::new();
-        new.push(k);
-        new.extend(self.0.iter().map(|k| k.clone()));
-        Path(new)
+        Path(Some((k, SharedPointer::new(self.clone()))))
     }
 
+    /// Adds `k` to the *back* of the list. O(self.length())
     pub fn snoc(&self, k: K) -> Self {
-        let mut new = self.0.clone();
-        new.push(k);
-        Path(new)
+        self.concat(&Path::root().cons(k))
     }
 }
