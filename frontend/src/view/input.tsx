4546bcc5baf9c92c30339ce4e8cfccf8cb6ae70a
Evan Patterson <evan@epatters.org>
2024-07-03 20:20:05 -0700

    ENH: Automatically resize inline `input`s to content.

--- frontend/src/view/input.tsx
+++ frontend/src/view/input.tsx
@@ -1,7 +1,26 @@
+import { createEffect, createSignal } from "solid-js";
 import { JSX } from "solid-js/jsx-runtime";
 
 import "./input.css";
 
-export const InlineInput = (props: JSX.InputHTMLAttributes<HTMLInputElement>) =>
-    <input class="inline-input" type="text" size="1" {...props}>
-    </input>
+
+export function InlineInput(props: JSX.InputHTMLAttributes<HTMLInputElement>) {
+    const [width, setWidth] = createSignal(0);
+
+    createEffect(() => {
+        let width = 0;
+        if (typeof props.value === "string" && props.value) {
+            width = props.value.length;
+        } else if (props.placeholder) {
+            width = props.placeholder.length;
+        }
+        setWidth(width);
+    });
+
+    return (
+        <input class="inline-input" type="text" size="1"
+               style={{ width: width() + 'ch' }}
+               {...props}>
+        </input>
+    );
+}
