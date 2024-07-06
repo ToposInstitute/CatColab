fdf3d4c1c8ba20647e57f42abdd97d26aa86b090
Evan Patterson <evan@epatters.org>
2024-07-05 20:18:48 -0700

    ENH: Sync outside of browser using Automerge's demo server.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
-import { Repo } from "@automerge/automerge-repo";
-import { createDoc } from "./automerge-solid";
+import { DocHandle, isValidAutomergeUrl, Repo } from "@automerge/automerge-repo";
+import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
+import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
 import { createShortcut } from "@solid-primitives/keyboard";
 
+import { createDoc } from "./automerge-solid";
 import { newFormalCell, Notebook } from "./model/notebook";
 import { ModelJudgment, newMorphismDecl, newObjectDecl } from "./model/model_judgments";
 import { NotebookEditorRef } from "./view/notebook_editor";
@@ -10,31 +12,43 @@ import { ModelEditor } from "./view/model_editor";
 import './App.css';
 
 
+
 function App() {
-  const repo = new Repo({});
-  const [notebook, modifyNotebook] = createDoc<Notebook<ModelJudgment>>(repo, {
+  const init: Notebook<ModelJudgment> = {
     name: "Untitled",
     cells: [],
+  };
+
+  const repo = new Repo({
+    storage: new IndexedDBStorageAdapter("catcolab-demo"),
+    network: [new BrowserWebSocketClientAdapter("wss://sync.automerge.org")],
   });
 
-  let nbRef!: NotebookEditorRef<ModelJudgment>;
+  const handleId = document.location.hash.substring(1);
+  let handle: DocHandle<Notebook<ModelJudgment>>;
+  if (isValidAutomergeUrl(handleId)) {
+    handle = repo.find(handleId);
+  } else {
+    handle = repo.create<Notebook<ModelJudgment>>(init);
+    document.location.hash = handle.url;
+  }
+
+  const [notebook, modifyNotebook] = createDoc(handle, init);
+
+  let editorRef!: NotebookEditorRef<ModelJudgment>;
 
   createShortcut(
     ["Alt", "0"],
-    () => nbRef.pushCell(newFormalCell(newObjectDecl("default"))),
+    () => editorRef.pushCell(newFormalCell(newObjectDecl("default"))),
   );
   createShortcut(
     ["Alt", "1"],
-    () => nbRef.pushCell(newFormalCell(newMorphismDecl("default"))),
+    () => editorRef.pushCell(newFormalCell(newMorphismDecl("default"))),
   );
 
   return (
-    <div>
-      <ModelEditor notebook={notebook()} modifyNotebook={modifyNotebook}
-        ref={(ref) => {nbRef = ref;}} />
-      <br/>
-      <ModelEditor notebook={notebook()} modifyNotebook={modifyNotebook} />
-    </div>
+    <ModelEditor notebook={notebook()} modifyNotebook={modifyNotebook}
+        ref={(ref) => {editorRef = ref;}} />
   );
 }
 
--- frontend/src/automerge-solid.ts
+++ frontend/src/automerge-solid.ts
@@ -1,17 +1,16 @@
-import { Repo } from "@automerge/automerge-repo";
+import { DocHandle } from "@automerge/automerge-repo";
 import { createStore, reconcile } from "solid-js/store";
 
-/** Create Solid-compatible getter/setter for an automerge document.
+/** Create Solid-compatible getter/setter from an Automerge document handle.
 
 Internally, a Solid Store is used along with the
 [`reconcile`](https://www.solidjs.com/tutorial/stores_immutable) function to
 diff the data and thus avoid re-rendering the whole DOM.
  */
 export function createDoc<T extends object>(
-  repo: Repo,
+  handle: DocHandle<T>,
   init: T,
 ): [() => T, (f: (d: T) => void) => void] {
-  const handle = repo.create(init);
   const [store, setStore] = createStore<T>(init);
 
   const get = () => store;
