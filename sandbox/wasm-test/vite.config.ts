4bc87284ed5e2c05fc701ba82f199ed63b83fba2
Evan Patterson <evan@epatters.org>
2024-07-14 21:59:23 -0700

    CLEANUP: Remove Rust-wasm test from sandbox.
    Rust-wasm is now integrated into the frontend itself.

--- sandbox/wasm-test/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import wasm from "vite-plugin-wasm";
-
-export default defineConfig({
-  plugins: [wasm(), solid()],
-});
