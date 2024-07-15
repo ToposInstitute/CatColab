ef32ca628dc94fb5cdf77b25da1b6639b07316bf
Evan Patterson <evan@epatters.org>
2024-07-14 21:01:34 -0700

    BUG: Rewrite inline input component to fix auto-sizing.
    The old approach only reliably worked for monospace fonts. The new
    approach works for any font.

--- frontend/src/components/inline_input.tsx
+++ frontend/src/components/inline_input.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal } from "solid-js";
+import { JSX } from "solid-js";
 import { focus } from "@solid-primitives/active-element";
 focus;
 
@@ -29,62 +29,47 @@ export function InlineInput(props: {
     text: string,
     setText: (text: string) => void;
 } & InlineInputOptions) {
-    const computeWidth = (text: string) => {
-        let width = 0;
-        if (text) {
-            width = text.length;
-        } else if (props.placeholder) {
-            width = props.placeholder.length;
+    const onKeyDown: JSX.EventHandlerUnion<HTMLInputElement, KeyboardEvent> = (evt) => {
+        const value = evt.currentTarget.value;
+        if (props.deleteBackward && evt.key === "Backspace" && !value) {
+            props.deleteBackward();
+        } else if (props.deleteForward && evt.key === "Delete" && !value) {
+            props.deleteForward();
+        } else if (props.exitBackward && evt.key === "Tab" && evt.shiftKey) {
+            props.exitBackward();
+        } else if (props.exitForward && evt.key === "Tab" && !evt.shiftKey) {
+            props.exitForward();
+        } else if (props.exitLeft && evt.key === "ArrowLeft" &&
+                   evt.currentTarget.selectionEnd == 0) {
+            props.exitLeft();
+        } else if (props.exitRight && evt.key === "ArrowRight" &&
+                   evt.currentTarget.selectionStart == value.length) {
+            props.exitRight();
+        } else if (props.exitUp && evt.key === "ArrowUp") {
+            props.exitUp();
+        } else if (props.exitDown && evt.key === "ArrowDown") {
+            props.exitDown();
+        } else {
+            return;
         }
-        return width;
+        evt.preventDefault();
     };
 
-    const [width, setWidth] = createSignal(0);
-
-    createEffect(() => {
-        setWidth(computeWidth(props.text));
-    });
-
-    return <input class="inline-input" type="text" size="1"
-        ref={props.ref}
-        classList={{ invalid: props.invalid }}
-        style={{ width: width() + 'ch' }}
-        value={props.text}
-        placeholder={props.placeholder}
-        use:focus={(isFocused) => {
-            isFocused && props.onFocus && props.onFocus();
-        }}
-        onInput={(evt) => {
-            let text = evt.target.value;
-            setWidth(computeWidth(text));
-            props.setText(text);
-        }}
-        onKeyDown={(evt) => {
-            const value = evt.currentTarget.value;
-            if (props.deleteBackward && evt.key === "Backspace" &&
-                value === "") {
-                props.deleteBackward();
-            } else if (props.deleteForward && evt.key === "Delete" &&
-                       value === "") {
-                props.deleteForward();
-            } else if (props.exitBackward && evt.key === "Tab" && evt.shiftKey) {
-                props.exitBackward();
-            } else if (props.exitForward && evt.key === "Tab" && !evt.shiftKey) {
-                props.exitForward();
-            } else if (props.exitLeft && evt.key === "ArrowLeft" &&
-                       evt.currentTarget.selectionEnd == 0) {
-                props.exitLeft();
-            } else if (props.exitRight && evt.key === "ArrowRight" &&
-                       evt.currentTarget.selectionStart == value.length) {
-                props.exitRight();
-            } else if (props.exitUp && evt.key === "ArrowUp") {
-                props.exitUp();
-            } else if (props.exitDown && evt.key === "ArrowDown") {
-                props.exitDown();
-            } else {
-                return;
-            }
-            evt.preventDefault();
-        }}
-    ></input>;
+    // Uses a hidden filler element: https://stackoverflow.com/a/41389961
+    return <div class="inline-input-container">
+        <span class="inline-input-filler">
+            {props.text || props.placeholder}
+        </span>
+        <input class="inline-input" type="text" size="1"
+            ref={props.ref}
+            classList={{ invalid: props.invalid }}
+            value={props.text}
+            placeholder={props.placeholder}
+            use:focus={(isFocused) => {
+                isFocused && props.onFocus && props.onFocus();
+            }}
+            onInput={(evt) => props.setText(evt.target.value)}
+            onKeyDown={onKeyDown}
+        ></input>
+    </div>;
 }
