814c1ae82e3591ccc9620f8698fc62efae0a02cb
Evan Patterson <evan@epatters.org>
2024-07-20 22:22:38 -0700

    ENH: First cut at models of a discrete double theory in Rust.

--- catlog/src/dbl/model.rs
+++ catlog/src/dbl/model.rs
@@ -35,15 +35,21 @@ In addition, a model has the following operations:
   whose type is the composite of the corresponding morphism types.
  */
 
-use crate::one::path::Path;
+use std::hash::Hash;
+use std::sync::Arc;
+
+use crate::zero::{Mapping, IndexedHashColumn};
+use crate::one::{Category, Path};
+use crate::one::fin_category::FpCategory;
+use super::theory::DblTheory;
 
 
 /** A model of a double theory.
 
-As always in logic, a model only makes sense relative to a theory, but a theory
-can have many different models. Thus, as an object in Rust, a model does not own
-the theory that it is a model of. Instead, it can be validated that a model is
-indeed valid for a theory provided separatedly.
+As always in logic, a model makes sense only relative to a theory, but a theory
+can have many different models. Thus, in Rust, a model generally needs access to
+its theory but should not own its theory. Implementors of this trait can use an
+immutable shared reference to the theory.
  */
 pub trait DblModel {
     /** Type of objects in the model.
@@ -78,6 +84,12 @@ pub trait DblModel {
     /// Does the model contain the value as a morphism?
     fn has_mor(&self, m: &Self::Mor) -> bool;
 
+    /// Type of object.
+    fn ob_type(&self, x: &Self::Ob) -> Self::ObType;
+
+    /// Type of morphism.
+    fn mor_type(&self, m: &Self::Mor) -> Self::MorType;
+
     /// Domain of morphism.
     fn dom(&self, m: &Self::Mor) -> Self::Ob;
 
@@ -102,4 +114,78 @@ pub trait DblModel {
 
     /// Acts on a morphism with a morphism operation.
     fn mor_act(&self, m: Self::Mor, α: &Self::MorOp) -> Self::Mor;
+}
+
+/** A finitely generated model of a double theory.
+
+Like the [finitely generated categories](crate::one::category::FgCategory) that
+it generalizes, a finitely generated models is rarely actually finite.
+ */
+pub trait FgDblModel: DblModel {
+    /// Is the object a generator? Implies `self.has_ob(x)`.
+    fn has_ob_generator(&self, x: &Self::Ob) -> bool;
+
+    /// Is the morphism a generator? Implies `self.has_mor(m)`.
+    fn has_mor_generator(&self, m: &Self::Mor) -> bool;
+
+    /// Iterates over object generators of the category.
+    fn ob_generators(&self) -> impl Iterator<Item = Self::Ob>;
+
+    /// Iterates over morphism generators of the category.
+    fn mor_generators(&self) -> impl Iterator<Item = Self::Mor>;
+
+    /// Iterates over morphism generators with the given domain.
+    fn generators_with_dom(&self, x: &Self::Ob) -> impl Iterator<Item = Self::Mor>;
+
+    /// Iterates over morphism generators with the given codomain.
+    fn generators_with_cod(&self, x: &Self::Ob) -> impl Iterator<Item = Self::Mor>;
+}
+
+
+/** A finitely presented model of a discrete double theory.
+
+Since the operations are trivial, such a model is a finite presentation of a
+category sliced over the object and morphism types comprising the double theory.
+A type theorist would call it a ["displayed
+category"](https://ncatlab.org/nlab/show/displayed+category).
+ */
+#[derive(Clone)]
+pub struct DiscreteDblModel<V,E,Th: DblTheory> {
+    theory: Arc<Th>,
+    category: FpCategory<V,E>,
+    ob_types: IndexedHashColumn<V,Th::ObType>,
+    mor_types: IndexedHashColumn<E,Th::MorType>,
+}
+
+impl<V,E,Th> DblModel for DiscreteDblModel<V,E,Th>
+where V: Eq+Clone+Hash, E: Eq+Clone+Hash, Th: DblTheory,
+      Th::ObType: Eq+Clone+Hash, Th::MorType: Eq+Clone+Hash {
+    type Ob = V;
+    type Mor = Path<V,E>;
+    type ObType = Th::ObType;
+    type MorType = Th::MorType;
+    type ObOp = Th::ObType;
+    type MorOp = Th::MorType;
+
+    fn has_ob(&self, x: &Self::Ob) -> bool { self.category.has_ob(x) }
+    fn has_mor(&self, m: &Self::Mor) -> bool { self.category.has_hom(m) }
+
+    fn ob_type(&self, x: &Self::Ob) -> Self::ObType {
+        self.ob_types.apply(x).expect("Object type should be set").clone()
+    }
+
+    fn mor_type(&self, m: &Self::Mor) -> Self::MorType {
+        let types = m.clone().map(|x| self.ob_type(&x), |n| {
+            self.mor_types.apply(&n).expect("Morphism type should be set").clone()
+        });
+        self.theory.compose_types(types)
+    }
+
+    fn dom(&self, m: &Self::Mor) -> Self::Ob { self.category.dom(m) }
+    fn cod(&self, m: &Self::Mor) -> Self::Ob { self.category.cod(m) }
+    fn compose(&self, path: Path<Self::Ob,Self::Mor>) -> Self::Mor {
+        self.category.compose(path)
+    }
+    fn ob_act(&self, x: Self::Ob, _: &Self::ObOp) -> Self::Ob { x }
+    fn mor_act(&self, m: Self::Mor, _: &Self::MorOp) -> Self::Mor { m }
 }
