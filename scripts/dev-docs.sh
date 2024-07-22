20e091a6bf8859cf22552cdab93e114cf13545a7
Owen Lynch <root@owenlynch.org>
2024-07-22 14:06:13 -0700

    use tinytex with minimal packages instead of texlive

--- scripts/dev-docs.sh
+++ scripts/dev-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-FORESTER_TARBALL = forester-4.1.0-x86_64-unknown-linux-musl.tar.gz
-FORESTER_URL = http://bafybeieju6aexd2iwqzgwpc2rfi4fkbogztphpfulr3k2wzyrhfvqxxa3m.ipfs.w3s.link/$FORESTER_TARBALL
+FORESTER_TARBALL=forester-4.1.0-x86_64-unknown-linux-musl.tar.gz
+FORESTER_URL=http://bafybeieju6aexd2iwqzgwpc2rfi4fkbogztphpfulr3k2wzyrhfvqxxa3m.ipfs.w3s.link/$FORESTER_TARBALL
 
 cd dev-docs
 curl -L -O $FORESTER_URL
