209b7ed473f629d9a4aeb6cf36c1fa788e10e468
Owen Lynch <root@owenlynch.org>
2024-07-22 11:44:29 -0700

    simplify devdocs script

--- scripts/devdocs.sh
+++ scripts/devdocs.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 FORESTER_VERSION=4.1.0
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null)
 
-cd SCRIPT_DIR
-tar -xf forester-$FORESTER_VERSION.tar.gz
-cd ..
 cd devdocs
-../scripts/forester build
+tar -xf ../scripts/forester-$FORESTER_VERSION.tar.gz
+./forester build
