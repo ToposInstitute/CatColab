1e7b3d95c329dfd17e35b568989b610a458132c2
Owen Lynch <root@owenlynch.org>
2024-07-22 11:43:28 -0700

    fix devdocs script

--- scripts/devdocs.sh
+++ scripts/devdocs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 FORESTER_VERSION=4.1.0
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null)
 
 cd SCRIPT_DIR
 tar -xf forester-$FORESTER_VERSION.tar.gz
