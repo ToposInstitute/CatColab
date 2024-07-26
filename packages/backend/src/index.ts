6a75a2b04f04fc5f508323c9ca38402bbd021968
Owen Lynch <root@owenlynch.org>
2024-07-25 18:29:45 -0700

    started backend package

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (_request, _reply) {
+  return { hello: 'world' }
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
