ef531a04b70791e9a5a00e5305e1e75577eceb6f
Evan Patterson <evan@epatters.org>
2024-08-03 13:40:22 -0700

    DOC: Proper TypeScript doc comments for double theory metadata.

--- packages/frontend/src/theory/types.ts
+++ packages/frontend/src/theory/types.ts
@@ -3,25 +3,25 @@ import type { KbdKey } from "@solid-primitives/keyboard";
 import type { DblTheory, MorType, ObType } from "catlog-wasm";
 import type { ArrowStyle } from "../visualization/types";
 
-/** A double theory with frontend metadata.
+/** A double theory equipped with metadata for use in frontend.
  */
 export class TheoryMeta {
-    // Unique identifier of theory.
+    /** Unique identifier of theory. */
     readonly id!: TheoryId;
 
-    // Human-readable name for models of theory.
+    /** Human-readable name for models of theory. */
     readonly name!: string;
 
-    // Tooltip-length description of models of theory.
+    /** Short description of models of theory. */
     readonly description?: string;
 
-    // The underlying double theory.
+    /** Underlying double theory in the core. */
     readonly theory: DblTheory;
 
-    // Types in theory bound with metadata, to be displayed in this order.
+    /** Types in theory bound with metadata, to be displayed in this order. */
     readonly types: TypeMeta[];
 
-    // Whether models of the double theory are constrained to be free.
+    /** Whether models of the double theory are constrained to be free. */
     readonly onlyFree!: boolean;
 
     constructor(props: {
@@ -45,7 +45,7 @@ export class TheoryMeta {
         });
     }
 
-    bindType(meta: TypeMeta) {
+    private bindType(meta: TypeMeta) {
         const index = this.types.length;
         if (meta.tag === "ObType") {
             this.theory.setObTypeIndex(meta.obType, index);
@@ -55,11 +55,13 @@ export class TheoryMeta {
         this.types.push(meta);
     }
 
+    /** Get metadata associated with object type. */
     getObTypeMeta(typ: ObType): ObTypeMeta | undefined {
         const i = this.theory.obTypeIndex(typ);
         return i != null ? (this.types[i] as ObTypeMeta) : undefined;
     }
 
+    /** Get metadata associated with morphism type. */
     getMorTypeMeta(typ: MorType): MorTypeMeta | undefined {
         const i = this.theory.morTypeIndex(typ);
         return i != null ? (this.types[i] as MorTypeMeta) : undefined;
@@ -68,43 +70,49 @@ export class TheoryMeta {
 
 export type TheoryId = string;
 
-/** A type in a double theory with frontend metadata.
+/** A type in a double theory equipped with frontend metadata.
  */
 export type TypeMeta = ObTypeMeta | MorTypeMeta;
 
-type BaseTypeMeta = {
-    // Human-readable name of type.
+/** Frontend metadata applicable to any type in a double theory.
+ */
+export type BaseTypeMeta = {
+    /** Human-readable name of type. */
     name: string;
 
-    // Tooltip-length description of type.
+    /** Short description of type. */
     description?: string;
 
-    // Keyboard shortcut for type, excluding modifier.
+    /** Keyboard shortcut for type, excluding modifier. */
     shortcut?: KbdKey[];
 
-    // CSS classes to apply to HTML displays.
+    /** CSS classes to apply to HTML displays. */
     cssClasses?: string[];
 
-    // CSS classes to apply to SVG displays.
+    /** CSS classes to apply to SVG displays. */
     svgClasses?: string[];
 
-    // CSS classes to apply to text in both HTML and SVG.
+    /** CSS classes to apply to text in both HTML and SVG. */
     textClasses?: string[];
 };
 
+/** Frontend metadata for object types in a double theory.
+ */
 export type ObTypeMeta = BaseTypeMeta & {
     tag: "ObType";
 
-    // Object type in underlying theory.
+    /** Object type in underlying theory. */
     obType: ObType;
 };
 
+/** Frontend metadata for morphism types in a double theory.
+ */
 export type MorTypeMeta = BaseTypeMeta & {
     tag: "MorType";
 
-    // Morphism type in underlying theory.
+    /** Morphism type in underlying theory. */
     morType: MorType;
 
-    // Style of arrow to use for morphisms of this type.
+    /** Style of arrow to use for morphisms of this type. */
     arrowStyle?: ArrowStyle;
 };
