a9eb96af8f4bf47891d51a17cbd867eeae9db7d2
Evan Patterson <evan@epatters.org>
2024-08-05 17:21:35 -0700

    ENH: Simple data structure for a mapping b/w discrete dbl theories.

--- packages/catlog/src/dbl/model_morphism.rs
+++ packages/catlog/src/dbl/model_morphism.rs
@@ -1,7 +1,11 @@
 //! Morphisms between models of double theories.
 
-use crate::zero::HashColumn;
+use std::hash::Hash;
+
+use derivative::Derivative;
+
 use crate::one::Path;
+use crate::zero::{HashColumn, Mapping};
 
 /** A mapping between models of a double theory.
 
@@ -10,16 +14,16 @@ Analogous to a mapping between [sets](crate::zero::Mapping) or
 of a double theory without specified domain or codomain models.
  */
 pub trait DblModelMapping {
-    /// Type of objects in domain model.
+    /// Type of objects in the domain model.
     type DomOb: Eq;
 
-    /// Type of morphisms in domain model.
+    /// Type of morphisms in the domain model.
     type DomMor: Eq;
 
-    /// Type of objects in codomain model.
+    /// Type of objects in the codomain model.
     type CodOb: Eq;
 
-    /// Type of morphisms in codomain model.
+    /// Type of morphisms in the codomain model.
     type CodMor: Eq;
 
     /// Applies the mapping to an object in the domain model.
@@ -27,4 +31,33 @@ pub trait DblModelMapping {
 
     /// Applies the mapping to a morphism in the domain model.
     fn apply_mor(&self, m: &Self::DomMor) -> Option<Self::CodMor>;
+}
+
+/// A mapping between models of a discrete double theory.
+#[derive(Clone, Derivative)]
+#[derivative(Default(bound = ""))]
+pub struct DiscreteDblModelMapping<DomId, CodId> {
+    ob_map: HashColumn<DomId, CodId>,
+    mor_map: HashColumn<DomId, Path<CodId, CodId>>,
+}
+
+impl<DomId, CodId> DblModelMapping for DiscreteDblModelMapping<DomId, CodId>
+where
+    DomId: Clone + Eq + Hash,
+    CodId: Clone + Eq + Hash,
+{
+    type DomOb = DomId;
+    type DomMor = Path<DomId, DomId>;
+    type CodOb = CodId;
+    type CodMor = Path<CodId, CodId>;
+
+    fn apply_ob(&self, x: &Self::DomOb) -> Option<Self::CodOb> {
+        self.ob_map.apply(x).cloned()
+    }
+
+    fn apply_mor(&self, m: &Self::DomMor) -> Option<Self::CodMor> {
+        m.clone()
+            .partial_map(|x| self.apply_ob(&x), |e| self.mor_map.apply(&e).cloned())
+            .map(|path| path.flatten())
+    }
 }
