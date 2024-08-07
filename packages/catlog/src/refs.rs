61558f0f463a6907c99cf675bb388a86410a87e2
Evan Patterson <evan@epatters.org>
2024-08-06 21:54:55 -0700

    DOC: Add remaining citations to references module in `catlog`.

--- packages/catlog/src/refs.rs
+++ packages/catlog/src/refs.rs
@@ -30,3 +30,38 @@ Patterson, 2024: Products in double categories, revisited.
 - [arXiv:2401.08990](https://arxiv.org/abs/2401.08990)
  */
 pub const DblProducts: () = ();
+
+/** Reference: Model structures for double categories.
+
+Fiore, Paoli, Pronk, 2008: Model structures on the category of small double
+categories.
+
+- [DOI:10.2140/agt.2008.8.1855](https://doi.org/10.2140/agt.2008.8.1855)
+- [arXiv:0711.0473](https://arxiv.org/abs/0711.0473)
+ */
+pub const ModelStructureDblCat: () = ();
+
+/** Reference: Word problem for double categories.
+
+Delpeuch, 2020: The word problem for double categories.
+
+- [TAC-35-1](http://www.tac.mta.ca/tac/volumes/35/1/35-01abs.html)
+- [arXiv:1907.09927](https://arxiv.org/abs/1907.09927)
+ */
+pub const WordProblemDblCats: () = ();
+
+/** Reference: Word problem for computads.
+
+Makkai, 2005: The word problem for computads.
+
+<https://www.math.mcgill.ca/makkai/WordProblem/>
+ */
+pub const MakkaiComputads: () = ();
+
+/** Reference: Computads and 2-dimensional pasting diagrams.
+
+Makkai, 2007: Computads and 2-dimensional pasting diagrams.
+
+<https://www.math.mcgill.ca/makkai/2dcomputads/>
+ */
+pub const MakkaiComputadsPasting: () = ();
