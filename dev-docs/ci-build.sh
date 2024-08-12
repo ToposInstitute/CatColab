e4d7c1e13ce93655929e0aa02d4305f49d61a201
TheCedarPrince <jacobszelko@gmail.com>
2024-08-12 17:56:45 -0400

    Added Docker build for 4.2 since it is not included by default yet

--- dev-docs/ci-build.sh
+++ dev-docs/ci-build.sh
@@ -4,5 +4,14 @@ FORESTER_URL=https://git.sr.ht/~jonsterling/ocaml-forester/archive/10e7c43441edd
 FORESTER_TARBALL=$(basename "$FORESTER_URL")
 
 curl -L -O $FORESTER_URL 
-tar -xf $FORESTER_TARBALL
+mkdir ocaml-forester
+tar -xf $FORESTER_TARBALL -C ocaml-forester --strip-components 1
+
+mv Dockerfile ocaml-forester
+mv static.patch ocaml-forester
+cd ocaml-forester
+docker build --output=docker-build --target=forester-built .
+mv docker-build/bin/forester ../
+cd ../
+
 ./forester build
