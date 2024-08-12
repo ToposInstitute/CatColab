a9bdf96566547363a3e81cd745b5466fed1bf799
TheCedarPrince <jacobszelko@gmail.com>
2024-08-12 17:12:25 -0400

    Accidentally screwed up pathing

--- dev-docs/ci-build.sh
+++ dev-docs/ci-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 FORESTER_URL=https://git.sr.ht/~jonsterling/ocaml-forester/archive/10e7c43441eddee5525f7967fa90496a4f1b4691.tar.gz
+FORESTER_TARBALL=$(basename "$FORESTER_URL")
 
-curl -L -O $FORESTER_URL
+curl -L -O $FORESTER_URL 
 tar -xf $FORESTER_TARBALL
 ./forester build
