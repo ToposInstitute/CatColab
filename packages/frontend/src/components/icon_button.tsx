b344226ed845f6b5eb2dc3f665b0c562d3beabfd
alexvcs <alexcurtisslep@gmail.com>
2024-09-02 14:20:43 -0400

    run npm run format and lint on the frontend

--- packages/frontend/src/components/icon_button.tsx
+++ packages/frontend/src/components/icon_button.tsx
@@ -1,28 +1,28 @@
-import {type JSX, splitProps} from "solid-js";
 import Tooltip from "@corvu/tooltip";
+import { type JSX, splitProps } from "solid-js";
 
 import "./icon_button.css";
 
 /** Styled, unobstrusive button intended to include an icon.
  */
 export function IconButton(
-  allProps: {
-    children: JSX.Element;
-  } & JSX.ButtonHTMLAttributes<HTMLButtonElement>
+    allProps: {
+        children: JSX.Element;
+    } & JSX.ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
-  const [props, buttonProps] = splitProps(allProps, ["children"]);
+    const [props, buttonProps] = splitProps(allProps, ["children"]);
 
-  return (
-    <Tooltip openOnHover>
-      <Tooltip.Trigger />
-      <Tooltip.Portal>
-        <Tooltip.Content data-corvu-tooltip-content={props}>
-          <button class="icon-button" {...buttonProps}>
-            {props.children}
-          </button>
-          <Tooltip.Arrow />
-        </Tooltip.Content>
-      </Tooltip.Portal>
-    </Tooltip>
-  );
+    return (
+        <Tooltip openOnHover>
+            <Tooltip.Trigger />
+            <Tooltip.Portal>
+                <Tooltip.Content data-corvu-tooltip-content={props}>
+                    <button class="icon-button" {...buttonProps}>
+                        {props.children}
+                    </button>
+                    <Tooltip.Arrow />
+                </Tooltip.Content>
+            </Tooltip.Portal>
+        </Tooltip>
+    );
 }
