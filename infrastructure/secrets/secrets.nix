d88aa034b8d41735255185a6e0103dab9705475f
Owen Lynch <root@owenlynch.org>
2024-09-12 17:32:09 -0700

    rekeyed DATABASE_URL.age for evan

--- infrastructure/secrets/secrets.nix
+++ infrastructure/secrets/secrets.nix
@@ -5,8 +5,10 @@ let
   };
 
   catcolab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOMBnXy4XMcogVMXdXt9NQCyAy8FaBj53groT4TTKBA root@ip-172-31-25-34.us-east-2.compute.internal";
+
+  epatters = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKXx6wMJSeYKCHNmbyR803RQ72uto9uYsHhAPPWNl2D evan@epatters.org";
 in
 builtins.mapAttrs (_: publicKeys: {inherit publicKeys;})
   ({
-      "DATABASE_URL.age" = [ red-special.o catcolab ];
+      "DATABASE_URL.age" = [ red-special.o catcolab epatters ];
   })
