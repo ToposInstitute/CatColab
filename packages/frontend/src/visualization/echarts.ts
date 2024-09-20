2a1728507264b45f63d2ee64b9bae65b3e9041d2
Evan Patterson <evan@epatters.org>
2024-09-20 13:17:18 -0700

    BUG: Prevent tree-shaking from breaking `echarts`.

--- packages/frontend/src/visualization/echarts.ts
+++ packages/frontend/src/visualization/echarts.ts
@@ -3,6 +3,11 @@
 @module
  */
 
+// XXX: Prevent echarts from being tree-shaken:
+// https://apache.github.io/echarts-handbook/en/basics/import/
+import * as echarts from "echarts";
+echarts;
+
 import { EChartsAutoSize } from "echarts-solid";
 
 export default EChartsAutoSize;
