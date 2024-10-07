3e99462ee661151f46c22ba9d59d1b6bd0b58012
Shaowei Lin <shaowei@gmail.com>
2024-10-07 11:41:27 -0700

    Add shaowei public key (#213)

--- infrastructure/secrets/secrets.nix
+++ infrastructure/secrets/secrets.nix
@@ -1,15 +1,12 @@
 
 let
   catcolab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOMBnXy4XMcogVMXdXt9NQCyAy8FaBj53groT4TTKBA root@ip-172-31-25-34.us-east-2.compute.internal";
-
-  red-special = {
-    o = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2sBTuqGoEXRWpBRqTBwZZPDdLGGJ0GQcuX5dfIZKb4 o@red-special";
-  };
-
+  owen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2sBTuqGoEXRWpBRqTBwZZPDdLGGJ0GQcuX5dfIZKb4 o@red-special";
   epatters = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKXx6wMJSeYKCHNmbyR803RQ72uto9uYsHhAPPWNl2D evan@epatters.org";
+  shaowei = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOV/7Vnjn7PwOC9VWyRAvsh5lUieIBHgdf4RRLkL8ZPa shaowei@gmail.com";
 in
 builtins.mapAttrs (_: publicKeys: {inherit publicKeys;})
   ({
-      "DATABASE_URL.age" = [ catcolab red-special.o epatters ];
-      "instrument.mjs.age" = [ catcolab red-special.o epatters ];
+      "DATABASE_URL.age" = [ catcolab owen epatters shaowei ];
+      "instrument.mjs.age" = [ catcolab owen epatters shaowei ];
   })
