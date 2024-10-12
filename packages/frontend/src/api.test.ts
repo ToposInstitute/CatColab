0786351be9bb331778eec8d738f3ee4cc2f6bc94
Evan Patterson <evan@epatters.org>
2024-10-11 17:31:02 -0700

    REFACTOR: Port new backend from `rspc` to `qubit`.
    I can't see how to do authentication with `rspc` but `qubit` has a
    well-documented story about auth.

--- packages/frontend/src/api.test.ts
+++ packages/frontend/src/api.test.ts
@@ -1,15 +1,15 @@
 import { type DocHandle, Repo, isValidDocumentId } from "@automerge/automerge-repo";
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
-import type { RSPCError } from "@rspc/client";
 import * as uuid from "uuid";
 import { assert, describe, test } from "vitest";
 
+import type { RpcResult } from "catcolab-api/src/RpcResult.ts";
 import { createRPCClient } from "./api.ts";
 
 const serverUrl: string = import.meta.env.VITE_SERVER_URL;
 const repoUrl: string = import.meta.env.VITE_AUTOMERGE_REPO_URL;
 
-const client = createRPCClient(serverUrl);
+const rpc = createRPCClient(serverUrl);
 const repo = new Repo({
     network: [new BrowserWebSocketClientAdapter(repoUrl)],
 });
@@ -17,35 +17,34 @@ const repo = new Repo({
 // XXX: Proper shutdown requires Automerge v2.
 //afterAll(() => repo.shutdown());
 
+function unwrap<T>(result: RpcResult<T>): T {
+    assert(result.tag === "Ok");
+    return result.content;
+}
+
 describe("Document RPC", async () => {
     const content = {
         type: "model",
         name: "My model",
     };
-    const refId = await client.mutation(["new_ref", content]);
+    const refId = unwrap(await rpc.new_ref.mutate(content));
     test.sequential("should get a valid UUID", () => {
         assert(uuid.validate(refId));
     });
 
-    const docId = await client.query(["doc_id", refId]);
+    const docId = unwrap(await rpc.doc_id.query(refId));
     test.sequential("should get a valid document ID", () => {
         assert(isValidDocumentId(docId));
     });
 
-    const newDocId = await client.query(["doc_id", refId]);
+    const newDocId = unwrap(await rpc.doc_id.query(refId));
     test.sequential("should get the same document ID as before", () => {
         assert.strictEqual(newDocId, docId);
     });
 
-    const badRefId = uuid.v7();
+    const result = await rpc.doc_id.query(uuid.v7());
     test("should get 404 when document does not exist", async () => {
-        try {
-            await client.query(["doc_id", badRefId]);
-        } catch (err) {
-            assert.strictEqual((err as RSPCError).code, 404);
-            return;
-        }
-        assert.fail();
+        assert(result.tag === "Err" && result.code === 404);
     });
 
     if (!isValidDocumentId(docId)) {
@@ -64,7 +63,7 @@ describe("Document RPC", async () => {
     });
 
     test.sequential("should update content in database", { timeout: 1000, retry: 5 }, async () => {
-        const newContent = await client.query(["head_snapshot", refId]);
+        const newContent = unwrap(await rpc.head_snapshot.query(refId)) as typeof content;
         assert.strictEqual(newContent.name, newName);
     });
 });
