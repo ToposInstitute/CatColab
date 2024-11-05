a9efd7b2af14360d5bc5ae106ccca9c767de8797
Hamidah Oderinwale <oderinwaleh@gmail.com>
2024-11-05 13:12:48 -0800

    package installs

---
+++ packages/frontend/src/analysis/export_visualization.tsx
@@ -0,0 +1,40 @@
+/**
+Get a handle to SVG DOM element once Solid has constructed it (using the ref mechanism)
+Use XMLSerializer to write that DOM node to a string
+Save that string as a file
+ */
+import { graphToDot, renderDot } from "render-dot"
+import computedStyleToInlineStyle from 'computed-style-to-inline-style';
+import { GraphvizSVG } from "../visualization";
+
+export function exportVisualizationSVG(visualization: HTMLDivElement) {
+    computedStyleToInlineStyle(visualization, { recursive: true });
+
+    // ref equivalent of get document element by ID 
+    var serializer = new XMLSerializer()
+    var source = serializer.serializeToString(visualization)
+     // Add name spaces
+     if (!source.match(/^<svg[^>]*?\sxmlns=(['"`])https?\:\/\/www\.w3\.org\/2000\/svg\1/)) {
+        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"')
+    }
+    if (!source.match(/^<svg[^>]*?\sxmlns:xlink=(['"`])http\:\/\/www\.w3\.org\/1999\/xlink\1/)) {
+        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"')
+    }
+    // Add xml declaration
+    source = '<?xml version="1.0" standalone="no"?>\r\n' + source
+
+    // Convert SVG source to URI data scheme
+    const url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source)
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "visualization.svg"
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    console.log("SVG file download initiated");
+
+}
+
