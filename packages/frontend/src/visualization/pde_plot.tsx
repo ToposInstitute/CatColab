4a5df3b22e033a48deba52e1c7869b02e156bd6d
Matt <cuffaro.matt@gmail.com>
2024-11-25 06:01:21 -0500

    fixed formatting, linting, removed comments

--- packages/frontend/src/visualization/pde_plot.tsx
+++ packages/frontend/src/visualization/pde_plot.tsx
@@ -1,7 +1,7 @@
+import { createTimer } from "@solid-primitives/timer";
 import type { EChartsOption } from "echarts";
 import { Match, Switch, lazy } from "solid-js";
 import { createSignal } from "solid-js";
-import { createTimer } from "@solid-primitives/timer";
 
 import type { JsResult } from "catlog-wasm";
 import { ErrorAlert } from "../components";
@@ -55,63 +55,62 @@ export function PDEPlot2D(props: {
     const maxValue = (): number =>
         props.data.state.map((data) => data.map((triple) => triple[2]).reduce(max)).reduce(max);
 
-	const timeLength = props.data.times.length;
-	// console.log(timeLength);
+    const timeLength = props.data.times.length;
 
-	// timer
-	let [count, setCount] = createSignal(0);
-	createTimer(() => setCount((count() + 5) % timeLength), 0.5, setInterval);
+    // timer
+    const [count, setCount] = createSignal(0);
+    createTimer(() => setCount((count() + 5) % timeLength), 0.5, setInterval);
 
-    function options(idx: number): EChartsOption { 
-	  // console.log(props.data.state[idx][1]);
-	  return {
-        xAxis: {
-            type: "category",
-            data: props.data.x,
-        },
-        yAxis: {
-            type: "category",
-            data: props.data.y,
-        },
-        visualMap: {
-            min: minValue(),
-            max: maxValue(),
-            calculable: false,
-            realtime: false,
-            inRange: {
-                // Source for colors:
-                // https://echarts.apache.org/examples/en/editor.html?c=heatmap-large
-                color: [
-                    "#313695",
-                    "#4575b4",
-                    "#74add1",
-                    "#abd9e9",
-                    "#e0f3f8",
-                    "#ffffbf",
-                    "#fee090",
-                    "#fdae61",
-                    "#f46d43",
-                    "#d73027",
-                    "#a50026",
-                ],
+    function options(idx: number): EChartsOption {
+        return {
+            xAxis: {
+                type: "category",
+                data: props.data.x,
             },
-        },
-        series: [
-            {
-                name: "Value",
-                type: "heatmap",
-                data: props.data.state[idx],
-                emphasis: {
-                    itemStyle: {
-                        borderColor: "black",
-                        borderWidth: 1,
-                    },
+            yAxis: {
+                type: "category",
+                data: props.data.y,
+            },
+            visualMap: {
+                min: minValue(),
+                max: maxValue(),
+                calculable: false,
+                realtime: false,
+                inRange: {
+                    // Source for colors:
+                    // https://echarts.apache.org/examples/en/editor.html?c=heatmap-large
+                    color: [
+                        "#313695",
+                        "#4575b4",
+                        "#74add1",
+                        "#abd9e9",
+                        "#e0f3f8",
+                        "#ffffbf",
+                        "#fee090",
+                        "#fdae61",
+                        "#f46d43",
+                        "#d73027",
+                        "#a50026",
+                    ],
                 },
-                progressive: false,
-                animation: false,
             },
-        ],
-    }};
+            series: [
+                {
+                    name: "Value",
+                    type: "heatmap",
+                    data: props.data.state[idx],
+                    emphasis: {
+                        itemStyle: {
+                            borderColor: "black",
+                            borderWidth: 1,
+                        },
+                    },
+                    progressive: false,
+                    animation: false,
+                },
+            ],
+        };
+    }
 
     return (
         <div class="plot">
