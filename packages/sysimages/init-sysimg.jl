c9145b6b13ad8feb818b685f346fe00d2ad2b512
Matt Cuffaro <cuffaro.matt@gmail.com>
2024-12-13 15:25:14 -0500

    added test precompilation statements

--- packages/sysimages/init-sysimg.jl
+++ packages/sysimages/init-sysimg.jl
@@ -10,14 +10,20 @@ Pkg.develop(path=joinpath(@__DIR__, "../algjulia-service"))
 
 @info "Creating the sysimage. This may take a while..."
 sysimg="AlgebraicJuliaService.so"
-create_sysimage(["AlgebraicJuliaService"], sysimage_path=sysimg)
+create_sysimage(["AlgebraicJuliaService"], sysimage_path=sysimg,
+			   precompile_execution_file="precompile.jl")
 sysimg_path=joinpath(@__DIR__, sysimg);
 
 @info "Adding $sysimg_path to IJulia kernel"
 Pkg.activate(); Pkg.add("IJulia")
 using IJulia
 
-installkernel("Julia AJaaSKernel", "--sysimage=$sysimg_path")
+# let's check if we have a kernel
+if any(!isnothing, match.(r"ajaaskernel", readdir(IJulia.kerneldir(), join=true)))
+	@info "Julia kernel which uses AlgebraicJuliaService sysimage already installed. Skipping..."
+else 
+	installkernel("Julia AJaaSKernel", "--sysimage=$sysimg_path")
+end
 
 @info "done!"
 exit()
