25957016e4621f0aa41f40a7571bd1d347ef08ad
Evan Patterson <evan@epatters.org>
2025-01-02 13:34:55 -0800

    ENH: Consistently support a title in `Foldable`, with chevron near it.

--- packages/frontend/src/components/foldable.tsx
+++ packages/frontend/src/components/foldable.tsx
@@ -1,7 +1,8 @@
 import Discloure from "@corvu/disclosure";
-import ChevronDown from "lucide-solid/icons/chevron-down";
-import ChevronUp from "lucide-solid/icons/chevron-up";
 import { type JSX, Show, createSignal } from "solid-js";
+
+import ChevronDown from "lucide-solid/icons/chevron-down";
+import ChevronRight from "lucide-solid/icons/chevron-right";
 
 import "./foldable.css";
 
@@ -11,7 +12,11 @@ This component is a styled wrapper around corvu's `Disclosure`. We could likely
 just as well have used kobalte's `Collapsible`.
  */
 export function Foldable(props: {
+    /** Title for the component, shown next to the fold/expand button. */
+    title?: string | JSX.Element;
+    /** Additional header content. */
     header?: JSX.Element;
+    /** Content that is expanded or folded. */
     children: JSX.Element;
 }) {
     const [isExpanded, setIsExpanded] = createSignal(false);
@@ -20,12 +25,18 @@ export function Foldable(props: {
     return (
         <Discloure expanded={isExpanded()} onExpandedChange={setIsExpanded} collapseBehavior="hide">
             <div class="foldable-header">
-                {props.header}
                 <Discloure.Trigger>
-                    <Show when={isExpanded()} fallback={<ChevronDown />}>
-                        <ChevronUp />
+                    <Show when={isExpanded()} fallback={<ChevronRight />}>
+                        <ChevronDown />
+                    </Show>
+                    <Show when={props.title}>
+                        <span class="title">{props.title}</span>
                     </Show>
                 </Discloure.Trigger>
+                <Show when={props.header}>
+                    <span class="filler" />
+                    {props.header}
+                </Show>
             </div>
             <Discloure.Content>{props.children}</Discloure.Content>
         </Discloure>
