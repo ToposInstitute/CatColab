024c83a8803b97aa29ccafab18637ce5fe11d6c2
Evan Patterson <evan@epatters.org>
2025-01-03 17:31:39 -0800

    ENH: RPC methods to get and set user profile.
    Most of the code is just validating the user name.

--- packages/backend/src/app.rs
+++ packages/backend/src/app.rs
@@ -20,7 +20,7 @@ pub struct AppState {
 /// Context available to RPC procedures.
 #[derive(Clone)]
 pub struct AppCtx {
-    /// Application state;
+    /// Application state.
     pub state: AppState,
 
     /// Authenticated Firebase user, if any.
@@ -30,15 +30,24 @@ pub struct AppCtx {
 /// Top-level application error.
 #[derive(Error, Debug)]
 pub enum AppError {
+    /// Error from the SQL database.
     #[error("SQL database error: {0}")]
     Db(#[from] sqlx::Error),
 
+    /// Error from the socket communicating with the Automerge document server.
     #[error("Error receiving acknowledgment from socket: {0}")]
     Ack(#[from] socketioxide::AckError<()>),
 
+    /// Client made request with invalid data.
+    #[error("Request with invalid data: {0}")]
+    Invalid(String),
+
+    /// Client has not authenticated using Firebase auth.
     #[error("Authentication credentials were not provided")]
     Unauthorized,
 
+    /// Client does not have permission to perform the requested action on the
+    /// document ref.
     #[error("Not authorized to access ref: {0}")]
     Forbidden(Uuid),
 }
