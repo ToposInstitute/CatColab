52219a56fb8057a86c50ad5aa6c707bf08aaa275
hamidahoderinwale <oderinwaleh@gmail.com>
2025-01-15 15:24:56 -0800

    imports back to original section

--- packages/frontend/src/page/menubar.tsx
+++ packages/frontend/src/page/menubar.tsx
@@ -4,16 +4,16 @@ import { getAuth, signOut } from "firebase/auth";
 import { useAuth, useFirebaseApp } from "solid-firebase";
 import { type JSX, Show, createSignal } from "solid-js";
 
+import { Dialog, IconButton } from "../components";
+import { Login } from "../user";
+
 import Info from "lucide-solid/icons/info";
 import LogInIcon from "lucide-solid/icons/log-in";
 import LogOutIcon from "lucide-solid/icons/log-out";
 import MenuIcon from "lucide-solid/icons/menu";
 import SettingsIcon from "lucide-solid/icons/settings";
-import { Dialog, IconButton } from "../components";
-import { Login } from "../user";
 
 import "./menubar.css";
-
 /** Menu triggered from a hamburger button. */
 export function HamburgerMenu(props: {
     children: JSX.Element;
