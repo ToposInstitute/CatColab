277382c87baf8dc88e3a6f2954879174ef41fcba
Kevin Carlson <kevin@topos.institute>
2025-01-17 17:15:35 -0800

    Add diagram import-export

--- packages/frontend/src/page/menubar.tsx
+++ packages/frontend/src/page/menubar.tsx
@@ -13,12 +13,14 @@ import { Dialog, IconButton } from "../components";
 import { Login } from "../user";
 import { Import } from "./import";
 
-import FilePlus from "lucide-solid/icons/file-plus";
-import Info from "lucide-solid/icons/info";
-import LogInIcon from "lucide-solid/icons/log-in";
-import LogOutIcon from "lucide-solid/icons/log-out";
-import MenuIcon from "lucide-solid/icons/menu";
-import SettingsIcon from "lucide-solid/icons/settings";
+import {
+    FilePlus,
+    LogInIcon,
+    LogOutIcon,
+    MenuIcon,
+    SettingsIcon,
+    UploadIcon,
+} from "lucide-solid";
 
 import "./menubar.css";
 
@@ -78,7 +80,7 @@ export function AppMenu(props: {
                     <SettingsMenuItem />
                     <LogOutMenuItem />
                 </Show>
-                <OpenMenuItem showOpen={() => setImportOpen(true)} />
+                <ImportMenuItem showImport={() => setImportOpen(true)} />
             </HamburgerMenu>
             <Dialog open={loginOpen()} onOpenChange={setLoginOpen} title="Log in">
                 <Login onComplete={() => setLoginOpen(false)} />
@@ -163,12 +165,12 @@ function SettingsMenuItem() {
     );
 }
 
-function OpenMenuItem(props: {
-    showOpen: () => void;
+function ImportMenuItem(props: {
+    showImport: () => void;
 }) {
     return (
-        <MenuItem onSelect={props.showOpen}>
-            <LogInIcon />
+        <MenuItem onSelect={props.showImport}>
+            <UploadIcon />
             <MenuItemLabel>{"Import notebook"}</MenuItemLabel>
         </MenuItem>
     );
