c5a07fe3158cfe691c2f96d52ef57a831bd76e4b
Kevin Carlson <kevin@topos.institute>
2025-01-17 17:15:35 -0800

    Add diagram import-export

--- packages/frontend/src/diagram/diagram_menu.tsx
+++ packages/frontend/src/diagram/diagram_menu.tsx
@@ -11,6 +11,8 @@ import {
     createDiagramFromDocument,
 } from "./document";
 
+import { copyToClipboard, downloadJson } from "../util/json_export";
+
 import ChartSpline from "lucide-solid/icons/chart-spline";
 import Copy from "lucide-solid/icons/copy";
 import FilePlus from "lucide-solid/icons/file-plus";
@@ -59,7 +61,13 @@ export function DiagramMenuItems(props: {
         navigate(`/diagram/${newRef}`);
     };
 
-    // XX: add JSON and copy menu items for diagrams? A bit repetitive, no?
+    //Can this be less repetitive?
+    const onDownloadJSON = (diagram: DiagramDocument) => {
+        downloadJson({ data: JSON.stringify(diagram), filename: `${diagram.name}.json` });
+    };
+    const onCopy = (diagram: DiagramDocument) => {
+        copyToClipboard({ data: JSON.stringify(diagram) });
+    };
     return (
         <>
             <MenuItem onSelect={() => onNewDiagram(props.liveDiagram.liveModel.refId)}>
@@ -74,6 +82,14 @@ export function DiagramMenuItems(props: {
             <MenuItem onSelect={() => onDuplicateDiagram(props.liveDiagram.liveDoc.doc)}>
                 <Copy />
                 <MenuItemLabel>{"Duplicate diagram"}</MenuItemLabel>
+            </MenuItem>
+            <MenuItem onSelect={() => onDownloadJSON(props.liveDiagram.liveDoc.doc)}>
+                <Copy />
+                <MenuItemLabel>{"Download JSON"}</MenuItemLabel>
+            </MenuItem>
+            <MenuItem onSelect={() => onCopy(props.liveDiagram.liveDoc.doc)}>
+                <Copy />
+                <MenuItemLabel>{"Copy to clipboard"}</MenuItemLabel>
             </MenuItem>
         </>
     );
