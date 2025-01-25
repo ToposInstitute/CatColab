74164b61589725934d62e7c80f487d6eb1f61764
Evan Patterson <evan@epatters.org>
2025-01-25 14:39:59 -0800

    ENH: Rings of polynomials.

--- packages/catlog/src/zero/alg.rs
+++ packages/catlog/src/zero/alg.rs
@@ -1,17 +1,296 @@
 //! Commutative algebra and polynomials.
 
-use super::rig::{CommRing, Module};
+use num_traits::{One, Pow, Zero};
+use std::fmt::Display;
+use std::iter::{Product, Sum};
+use std::ops::{Add, AddAssign, Mul, Neg};
 
-/** A commutative algebra over a commutative ring.
+use derivative::Derivative;
 
-TODO: As ring homomorphism.
- */
+use super::rig::*;
+
+/// A commutative algebra over a commutative ring.
 pub trait CommAlg: CommRing + Module<Ring = Self::R> {
     /// The base ring of the algebra.
     type R: CommRing;
 
-    /// Convert an element of the base ring into an element of the algebra.
+    /** Convert an element of the base ring into an element of the algebra.
+
+    A commutative algebra A over a commutative ring R can be defined as a ring
+    homomorphism from R to A. This method computes that homomorphism.
+     */
     fn from_scalar(r: Self::R) -> Self {
         Self::one() * r
+    }
+}
+
+/** A polynomial in several variables.
+
+TODO: As free algebras
+ */
+#[derive(Clone, PartialEq, Eq, Derivative)]
+#[derivative(Default(bound = ""))]
+pub struct Polynomial<Var, Coef, Exp>(Combination<Monomial<Var, Exp>, Coef>);
+
+impl<Var, Coef, Exp> Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Exp: Ord,
+{
+    /// Constructs the generating polynomial corresponding to a variable.
+    pub fn generator(var: Var) -> Self
+    where
+        Coef: One,
+        Exp: One,
+    {
+        Polynomial::from_monomial(Monomial::generator(var))
+    }
+
+    /// Constructs the polynomial corresponding to a monomial.
+    pub fn from_monomial(m: Monomial<Var, Exp>) -> Self
+    where
+        Coef: One,
+    {
+        Polynomial(Combination::generator(m))
+    }
+
+    /// Iterates over the monomials in the polynomial.
+    pub fn monomials(&self) -> impl ExactSizeIterator<Item = &Monomial<Var, Exp>> {
+        self.0.variables()
+    }
+
+    /// Evaluates the polynomial by substituting for the variables.
+    pub fn eval<A>(&self, values: &[A]) -> A
+    where
+        A: Clone + Mul<Coef, Output = A> + Pow<Exp, Output = A> + Sum + Product,
+        Coef: Clone,
+        Exp: Clone,
+    {
+        self.0.eval(self.monomials().map(|m| m.eval(values.iter().cloned())))
+    }
+}
+
+impl<Var, Coef, Exp> FromIterator<(Coef, Monomial<Var, Exp>)> for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Add<Output = Coef>,
+    Exp: Ord,
+{
+    fn from_iter<T: IntoIterator<Item = (Coef, Monomial<Var, Exp>)>>(iter: T) -> Self {
+        Polynomial(iter.into_iter().collect())
+    }
+}
+
+impl<Var, Coef, Exp> Display for Polynomial<Var, Coef, Exp>
+where
+    Var: Display,
+    Coef: Display + PartialEq + One,
+    Exp: Display + PartialEq + One,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+// XXX: Lots of boilerplate to delegate the module structure of `Polynomial` to
+// `Combination` because these traits cannot be derived automatically.
+
+impl<Var, Coef, Exp> AddAssign<(Coef, Monomial<Var, Exp>)> for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Add<Output = Coef>,
+    Exp: Ord,
+{
+    fn add_assign(&mut self, rhs: (Coef, Monomial<Var, Exp>)) {
+        self.0 += rhs;
+    }
+}
+
+impl<Var, Coef, Exp> Add for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Add<Output = Coef>,
+    Exp: Ord,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Polynomial(self.0 + rhs.0)
+    }
+}
+
+impl<Var, Coef, Exp> Zero for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Add<Output = Coef>,
+    Exp: Ord,
+{
+    fn zero() -> Self {
+        Polynomial(Combination::zero())
+    }
+
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+}
+
+impl<Var, Coef, Exp> AdditiveMonoid for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: AdditiveMonoid,
+    Exp: Ord,
+{
+}
+
+impl<Var, Coef, Exp> Mul<Coef> for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Clone + Default + Mul<Output = Coef>,
+    Exp: Ord,
+{
+    type Output = Self;
+
+    fn mul(self, a: Coef) -> Self::Output {
+        Polynomial(self.0 * a)
+    }
+}
+
+impl<Var, Coef, Exp> RigModule for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Clone + Default + CommRig,
+    Exp: Ord,
+{
+    type Rig = Coef;
+}
+
+impl<Var, Coef, Exp> Neg for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Default + Neg<Output = Coef>,
+    Exp: Ord,
+{
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Polynomial(self.0.neg())
+    }
+}
+
+impl<Var, Coef, Exp> AbGroup for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Default + AbGroup,
+    Exp: Ord,
+{
+}
+
+impl<Var, Coef, Exp> Module for Polynomial<Var, Coef, Exp>
+where
+    Var: Ord,
+    Coef: Clone + Default + CommRing,
+    Exp: Ord,
+{
+    type Ring = Coef;
+}
+
+/// Multiply polynomials using the distributive law.
+impl<Var, Coef, Exp> Mul for Polynomial<Var, Coef, Exp>
+where
+    Var: Clone + Ord,
+    Coef: Clone + Add<Output = Coef> + Mul<Output = Coef>,
+    Exp: Clone + Ord + Add<Output = Exp>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        // Avoid unnecessary clones by tracking whether we're in the last
+        // iteration of the outer and inner loops.
+        let mut result = Polynomial::zero();
+        let (outer, inner) = (self.0, rhs.0);
+        let mut outer_iter = outer.into_iter();
+        while let Some((a, m)) = outer_iter.next() {
+            if outer_iter.len() == 0 {
+                let mut inner_iter = inner.into_iter();
+                while let Some((b, n)) = inner_iter.next() {
+                    if inner_iter.len() == 0 {
+                        result += (a * b, m * n);
+                        break;
+                    } else {
+                        result += (a.clone() * b, m.clone() * n);
+                    }
+                }
+                break;
+            } else {
+                for (b, n) in &inner {
+                    result += (a.clone() * b.clone(), m.clone() * n.clone());
+                }
+            }
+        }
+        result
+    }
+}
+
+impl<Var, Coef, Exp> One for Polynomial<Var, Coef, Exp>
+where
+    Var: Clone + Ord,
+    Coef: Clone + Add<Output = Coef> + One,
+    Exp: Clone + Ord + Add<Output = Exp>,
+{
+    fn one() -> Self {
+        Polynomial::from_monomial(Default::default())
+    }
+}
+
+impl<Var, Coef, Exp> Monoid for Polynomial<Var, Coef, Exp>
+where
+    Var: Clone + Ord,
+    Coef: Clone + Rig,
+    Exp: Clone + Ord + AdditiveMonoid,
+{
+}
+
+impl<Var, Coef, Exp> Rig for Polynomial<Var, Coef, Exp>
+where
+    Var: Clone + Ord,
+    Coef: Clone + Rig,
+    Exp: Clone + Ord + AdditiveMonoid,
+{
+}
+
+impl<Var, Coef, Exp> CommMonoid for Polynomial<Var, Coef, Exp>
+where
+    Var: Clone + Ord,
+    Coef: Clone + CommRig,
+    Exp: Clone + Ord + AdditiveMonoid,
+{
+}
+
+impl<Var, Coef, Exp> CommRig for Polynomial<Var, Coef, Exp>
+where
+    Var: Clone + Ord,
+    Coef: Clone + CommRig,
+    Exp: Clone + Ord + AdditiveMonoid,
+{
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn polynomials() {
+        let x = || { Polynomial::<_, i32, u32>::generator('x') };
+        let y = || { Polynomial::<_, i32, u32>::generator('y') };
+        assert_eq!(x().to_string(), "x");
+
+        let p = x() * y() * x() * 2 + y() * x() * y() * 3;
+        assert_eq!(p.to_string(), "3 x y^2 + 2 x^2 y");
+        assert_eq!(p.eval(&[1, 1]), 5);
+        assert_eq!(p.eval(&[1, 2]), 16);
+        assert_eq!(p.eval(&[2, 1]), 14);
+
+        let p = (x() + y()) * (x() + y());
+        assert_eq!(p.to_string(), "2 x y + x^2 + y^2");
     }
 }
--- packages/catlog/src/zero/rig.rs
+++ packages/catlog/src/zero/rig.rs
@@ -3,15 +3,15 @@
 Lots of people have their own versions of a trait hierarchy for abstract
 algebra; see [`noether`](https://crates.io/crates/noether) and links therein.
 Our aim is not to make the most complete or general hierarchy but just to meet
-our own needs, which currently is a bit of [commutative algebra](super::alg),
+our own needs. Currently that is a bit of [commutative algebra](super::alg),
 especially polynomial algebras over rings. So we avoid slicing the salomi too
-thin with minor concepts like magmas and semigroups. We also take the category
-theorist's attitude that rigs are respectable concept that do not deserve to be
-denigrated as "semirings".
+thin with minor concepts like magmas and semigroups. We take the category
+theorist's attitude that rigs are a respectable concept that do not deserve to
+be called "semirings".
 
-Besides the hierarchy of traits, the data structure provided here for [linear
-combinations](Combination) and [monomials](Monomial). These are actually the
-same data structure, but with different notation!
+Besides the hierarchy of traits, this module provides data structures for
+[linear combinations](Combination) and [monomials](Monomial). These are actually
+the same data structure, but with different notation!
  */
 
 use num_traits::{One, Pow, Zero};
@@ -20,6 +20,7 @@ use std::fmt::Display;
 use std::iter::{Product, Sum};
 use std::ops::{Add, AddAssign, Mul, MulAssign, Neg};
 
+use derivative::Derivative;
 use duplicate::duplicate_item;
 
 /// A commutative monoid, written additively.
@@ -101,19 +102,25 @@ elements of the set.
 Combinations have exactly the same underlying data structure as
 [monomials](Monomial), but are written additively rather than multiplicatively.
  */
-#[derive(Clone, Default, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq, Derivative)]
+#[derivative(Default(bound = ""))]
 pub struct Combination<Var, Coef>(BTreeMap<Var, Coef>);
 
 impl<Var, Coef> Combination<Var, Coef>
 where
     Var: Ord,
 {
-    /// Generating combination on a single variable.
+    /// Constructs the generating combination corresponding to a variable.
     pub fn generator(var: Var) -> Self
     where
         Coef: One,
     {
-        [(Coef::one(), var)].into_iter().collect()
+        Combination([(var, Coef::one())].into_iter().collect())
+    }
+
+    /// Iterates over the variables used in the combination.
+    pub fn variables(&self) -> impl ExactSizeIterator<Item = &Var> {
+        self.0.keys()
     }
 
     /// Evaluates the combination by substituting for the variables.
@@ -126,13 +133,45 @@ where
     }
 }
 
-/// Constructs a combination from a sequence of coefficient-variable pairs.
+/// Constructs a combination from a list of terms (coefficient-variable pairs).
 impl<Var, Coef> FromIterator<(Coef, Var)> for Combination<Var, Coef>
 where
     Var: Ord,
+    Coef: Add<Output = Coef>,
 {
     fn from_iter<T: IntoIterator<Item = (Coef, Var)>>(iter: T) -> Self {
-        Combination(iter.into_iter().map(|(coef, var)| (var, coef)).collect())
+        let mut combination = Combination::zero();
+        for rhs in iter {
+            combination += rhs;
+        }
+        combination
+    }
+}
+
+/// Iterates over the terms (coefficient-variable pairs) of the polynomial.
+impl<Var, Coef> IntoIterator for Combination<Var, Coef> {
+    type Item = (Coef, Var);
+
+    type IntoIter = std::iter::Map<
+        std::collections::btree_map::IntoIter<Var, Coef>,
+        fn((Var, Coef)) -> (Coef, Var),
+    >;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter().map(|(var, coef)| (coef, var))
+    }
+}
+
+impl<'a, Var, Coef> IntoIterator for &'a Combination<Var, Coef> {
+    type Item = (&'a Coef, &'a Var);
+
+    type IntoIter = std::iter::Map<
+        std::collections::btree_map::Iter<'a, Var, Coef>,
+        fn((&'a Var, &'a Coef)) -> (&'a Coef, &'a Var),
+    >;
+
+    fn into_iter(self) -> Self::IntoIter {
+        (&self.0).into_iter().map(|(var, coef)| (coef, var))
     }
 }
 
@@ -173,27 +212,39 @@ where
     }
 }
 
+impl<Var, Coef> AddAssign<(Coef, Var)> for Combination<Var, Coef>
+where
+    Var: Ord,
+    Coef: Add<Output = Coef>,
+{
+    fn add_assign(&mut self, rhs: (Coef, Var)) {
+        let rhs = (rhs.1, rhs.0);
+        _add_assign(&mut self.0, rhs);
+    }
+}
+
 impl<Var, Coef> AddAssign for Combination<Var, Coef>
 where
     Var: Ord,
     Coef: Add<Output = Coef>,
 {
     fn add_assign(&mut self, rhs: Self) {
-        _add_assign(&mut self.0, rhs.0)
+        for rhs in rhs.0 {
+            _add_assign(&mut self.0, rhs);
+        }
     }
 }
 
-fn _add_assign<K, V>(lhs: &mut BTreeMap<K, V>, rhs: BTreeMap<K, V>)
+fn _add_assign<K, V>(lhs: &mut BTreeMap<K, V>, rhs: (K, V))
 where
     K: Ord,
     V: Add<Output = V>,
 {
-    for (var, b) in rhs {
-        if let Some(a) = lhs.remove(&var) {
-            lhs.insert(var, a + b);
-        } else {
-            lhs.insert(var, b);
-        }
+    let (k, b) = rhs;
+    if let Some(a) = lhs.remove(&k) {
+        lhs.insert(k, a + b);
+    } else {
+        lhs.insert(k, b);
     }
 }
 
@@ -302,19 +353,20 @@ monomials themselves become ordered under the lexicographic order. This is a
 valid *monomial ordering* as used in Groebner bases
 ([*IVA*](crate::refs::IdealsVarietiesAlgorithms), Section 2.2).
  */
-#[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Derivative)]
+#[derivative(Default(bound = ""))]
 pub struct Monomial<Var, Exp>(BTreeMap<Var, Exp>);
 
 impl<Var, Exp> Monomial<Var, Exp>
 where
     Var: Ord,
 {
-    /// Generating monomial on a single variable.
+    /// Constructs the generating monomial corresponding to a variable.
     pub fn generator(var: Var) -> Self
     where
         Exp: One,
     {
-        [(var, Exp::one())].into_iter().collect()
+        Monomial([(var, Exp::one())].into_iter().collect())
     }
 
     /// Evaluates the monomial by substituting for the variables.
@@ -331,9 +383,14 @@ where
 impl<Var, Exp> FromIterator<(Var, Exp)> for Monomial<Var, Exp>
 where
     Var: Ord,
+    Exp: Add<Output = Exp>,
 {
     fn from_iter<T: IntoIterator<Item = (Var, Exp)>>(iter: T) -> Self {
-        Monomial(iter.into_iter().collect())
+        let mut monomial = Monomial::one();
+        for rhs in iter {
+            monomial *= rhs;
+        }
+        monomial
     }
 }
 
@@ -370,13 +427,25 @@ where
     }
 }
 
+impl<Var, Exp> MulAssign<(Var, Exp)> for Monomial<Var, Exp>
+where
+    Var: Ord,
+    Exp: Add<Output = Exp>,
+{
+    fn mul_assign(&mut self, rhs: (Var, Exp)) {
+        _add_assign(&mut self.0, rhs);
+    }
+}
+
 impl<Var, Exp> MulAssign for Monomial<Var, Exp>
 where
     Var: Ord,
     Exp: Add<Output = Exp>,
 {
     fn mul_assign(&mut self, rhs: Self) {
-        _add_assign(&mut self.0, rhs.0)
+        for rhs in rhs.0 {
+            *self *= rhs;
+        }
     }
 }
 
@@ -427,12 +496,12 @@ mod tests {
 
     #[test]
     fn combinations() {
-        let x = Combination::generator('x');
-        let y = Combination::generator('y');
-        assert_eq!(x.to_string(), "x");
-        let combination = x.clone() * 2u32 + y.clone() * 3u32;
+        let x = || Combination::generator('x');
+        let y = || Combination::generator('y');
+        assert_eq!(x().to_string(), "x");
+        let combination = x() * 2u32 + y() * 3u32;
         assert_eq!(combination.to_string(), "2 x + 3 y");
-        assert_eq!((x.clone() + y.clone() + y + x).to_string(), "2 x + 2 y");
+        assert_eq!((x() + y() + y() + x()).to_string(), "2 x + 2 y");
         assert_eq!(combination.eval([5, 1].into_iter()), 13);
 
         assert_eq!(Combination::<char, u32>::zero().to_string(), "0");
@@ -444,10 +513,10 @@ mod tests {
 
     #[test]
     fn monomials() {
-        let x = Monomial::<_, u32>::generator('x');
-        let y = Monomial::<_, u32>::generator('y');
-        assert_eq!(x.clone().to_string(), "x");
-        assert_eq!((x.clone() * y.clone() * y * x).to_string(), "x^2 y^2");
+        let x = || Monomial::<_, u32>::generator('x');
+        let y = || Monomial::<_, u32>::generator('y');
+        assert_eq!(x().to_string(), "x");
+        assert_eq!((x() * y() * y() * x()).to_string(), "x^2 y^2");
 
         let monomial: Monomial<_, u32> = [('x', 1), ('y', 2)].into_iter().collect();
         assert_eq!(monomial.to_string(), "x y^2");
