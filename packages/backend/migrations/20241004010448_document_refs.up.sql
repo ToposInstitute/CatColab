b65ff5cc45317e2ae26b998bac69576e80a5a636
Topos <oderinwaleh@gmail.com>
2025-02-21 14:43:35 -0800

    ts_vector column updated automatically with triggers

--- packages/backend/migrations/20241004010448_document_refs.up.sql
+++ packages/backend/migrations/20241004010448_document_refs.up.sql
@@ -13,3 +13,10 @@ CREATE TABLE refs (
 
 ALTER TABLE snapshots
     ADD FOREIGN KEY (for_ref) REFERENCES refs (id) DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE snapshots 
+    ADD COLUMN fts_tsvector GENERATED ALWAYS AS (
+        TO_TSVECTOR('simple', COALESCE(content::text, ''))
+        ) STORED; 
+
+    
