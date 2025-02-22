57f6d92e753d62930e8b402b8b37ebb874e150c6
Evan Patterson <evan@epatters.org>
2025-02-22 12:20:22 -0800

    REFACTOR: Uniform interface for `InputField` and `TextAreaField`.

--- packages/frontend/src/components/form.tsx
+++ packages/frontend/src/components/form.tsx
@@ -28,7 +28,7 @@ export function InputField(allProps: InputFieldProps & Omit<ComponentProps<"inpu
             </dt>
             <dd>
                 <input {...inputProps} id={fieldId} />
-                <InputError error={props.error} />
+                <FieldError error={props.error} />
             </dd>
         </>
     );
@@ -40,12 +40,6 @@ export function TextInputField(
 ) {
     return <InputField type="text" {...props} />;
 }
-
-const InputError = (props: { error?: string }) => (
-    <Show when={props.error}>
-        <div class="error">{props.error}</div>
-    </Show>
-);
 
 /** Select field in a form group.
 
@@ -84,14 +78,10 @@ export function SelectField(
 }
 
 /** Text area field in a form group. */
-export function TextAreaField(
-    allProps: {
-        label: string | JSX.Element;
-    } & Omit<ComponentProps<"textarea">, "id">,
-) {
+export function TextAreaField(allProps: InputFieldProps & Omit<ComponentProps<"textarea">, "id">) {
     const fieldId = createUniqueId();
 
-    const [props, textProps] = splitProps(allProps, ["label"]);
+    const [props, textProps] = splitProps(allProps, ["label", "error"]);
 
     return (
         <>
@@ -100,7 +90,15 @@ export function TextAreaField(
             </dt>
             <dd>
                 <textarea id={fieldId} {...textProps} />
+                <FieldError error={props.error} />
             </dd>
         </>
     );
 }
+
+/** Validation error for a field. */
+const FieldError = (props: { error?: string }) => (
+    <Show when={props.error}>
+        <div class="error">{props.error}</div>
+    </Show>
+);
