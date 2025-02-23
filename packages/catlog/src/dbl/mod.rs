fef350e1898cfc7122c13bf3c53d422455310799
Evan Patterson <evan@epatters.org>
2025-02-22 21:49:52 -0800

    REFACTOR: Remove WIP data structures for double computads & diagrams.
    I am moving to use virtual double categories instead, which will be
    based on different data structures.

--- packages/catlog/src/dbl/mod.rs
+++ packages/catlog/src/dbl/mod.rs
@@ -1,9 +1,5 @@
 //! Double category theory and two-dimensional categorical logic.
 
-pub mod computad;
-pub mod diagram;
-pub mod pasting;
-
 pub mod model;
 pub mod model_diagram;
 pub mod model_morphism;
