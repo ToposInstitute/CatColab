efcae61e1d3a88007dcaa40afeed139c2a8e8178
Jason Moggridge <github_hou1@jmoggr.com>
2025-03-03 10:41:56 -0500

    Automerge+prosemirror example using a text field in a custom node

--- packages/automerge-doc-server/src/server.ts
+++ packages/automerge-doc-server/src/server.ts
@@ -83,6 +83,7 @@ export class AutomergeServer {
         }
 
         handle.on("change", async (payload) => {
+            console.log(payload.patches);
             this.handleChange?.(refId, payload.doc);
         });
 
