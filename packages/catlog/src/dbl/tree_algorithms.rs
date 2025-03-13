92794cd3ea057ad57e572f0fbf8c2559bc5dd87f
Evan Patterson <evan@epatters.org>
2025-03-12 21:19:44 -0700

    ENH: Allow peeking at next node at same level in tree BFS.

--- packages/catlog/src/dbl/tree_algorithms.rs
+++ packages/catlog/src/dbl/tree_algorithms.rs
@@ -1,19 +1,87 @@
 //! Generic algorithms on [trees](Tree).
 
-use ego_tree::{NodeRef, Tree, iter::Edge};
+use ego_tree::iter::{Descendants, Edge};
+use ego_tree::{NodeId, NodeRef, Tree};
 use std::collections::VecDeque;
 
 /// Extension trait adding traversal algorithms on [trees](Tree).
 pub trait TreeTraversal<T> {
     /// Iterates over nodes of a tree in depth-first order.
-    fn dfs<'a>(&'a self) -> impl Iterator<Item = NodeRef<'a, T>>
+    fn dfs<'a>(&'a self) -> Descendants<'a, T>
     where
         T: 'a;
 
     /// Iterates over the nodes in a tree in breadth-first order.
-    fn bfs<'a>(&'a self) -> impl Iterator<Item = NodeRef<'a, T>>
+    fn bfs<'a>(&'a self) -> BreadthFirstTraversal<'a, T>
     where
         T: 'a;
+}
+
+/// Iterator for traversing a tree in breadth-first order.
+pub struct BreadthFirstTraversal<'a, T: 'a> {
+    tree: &'a Tree<T>,
+    queue: VecDeque<(NodeId, usize)>,
+    current_level: usize,
+}
+
+impl<'a, T: 'a> BreadthFirstTraversal<'a, T> {
+    /// Initialize a breadth-first traversal at the given node.
+    pub fn starting_at(root: NodeRef<'a, T>) -> Self {
+        let tree = root.tree();
+        let mut queue = VecDeque::new();
+        queue.push_back((root.id(), 1));
+        Self {
+            tree,
+            queue,
+            current_level: 0,
+        }
+    }
+
+    /// Peeks at the next node, if it's at the same level as the previous one.
+    pub fn peek_at_same_level(&self) -> Option<NodeRef<'a, T>> {
+        self.queue.front().and_then(|(id, level)| {
+            if *level == self.current_level {
+                self.tree.get(*id)
+            } else {
+                None
+            }
+        })
+    }
+}
+
+impl<'a, T: 'a> Iterator for BreadthFirstTraversal<'a, T> {
+    type Item = NodeRef<'a, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (id, level) = self.queue.pop_front()?;
+        self.current_level = level;
+        let node = self.tree.get(id).unwrap();
+        for child in node.children() {
+            self.queue.push_back((child.id(), level + 1));
+        }
+        Some(node)
+    }
+}
+
+impl<'a, T: 'a> std::iter::FusedIterator for BreadthFirstTraversal<'a, T> {}
+
+impl<T> TreeTraversal<T> for Tree<T> {
+    /// Uses the built-in traversal algorithm, which is depth-first, though that
+    /// is not documented: <https://github.com/rust-scraper/ego-tree/issues/38>
+    fn dfs<'a>(&'a self) -> Descendants<'a, T>
+    where
+        T: 'a,
+    {
+        self.root().descendants()
+    }
+
+    /// Implements the standard BFS algorithm using a queue.
+    fn bfs<'a>(&'a self) -> BreadthFirstTraversal<'a, T>
+    where
+        T: 'a,
+    {
+        BreadthFirstTraversal::starting_at(self.root())
+    }
 }
 
 /// Extension trait adding isomorphism checking on [trees](Tree).
@@ -26,34 +94,6 @@ pub trait TreeIsomorphism<T> {
     underlying data that is equal.
      */
     fn is_isomorphic_to(&self, other: &Self) -> bool;
-}
-
-impl<T> TreeTraversal<T> for Tree<T> {
-    /// Uses the built-in traversal algorithm, which is depth-first, though that
-    /// is not documented: <https://github.com/rust-scraper/ego-tree/issues/38>
-    fn dfs<'a>(&'a self) -> impl Iterator<Item = NodeRef<'a, T>>
-    where
-        T: 'a,
-    {
-        self.root().descendants()
-    }
-
-    /// Implements the standard BFS algorithm using a queue.
-    fn bfs<'a>(&'a self) -> impl Iterator<Item = NodeRef<'a, T>>
-    where
-        T: 'a,
-    {
-        let mut queue = VecDeque::new();
-        queue.push_back(self.root().id());
-        std::iter::from_fn(move || {
-            let id = queue.pop_front()?;
-            let node = self.get(id).unwrap();
-            for child in node.children() {
-                queue.push_back(child.id());
-            }
-            Some(node)
-        })
-    }
 }
 
 impl<T> TreeIsomorphism<T> for Tree<T>
@@ -96,6 +136,13 @@ mod tests {
         let tree = tree!('a' => { 'b' => { 'd', 'e' }, 'c' });
         let values: Vec<_> = tree.bfs().map(|node| *node.value()).collect();
         assert_eq!(values, vec!['a', 'b', 'c', 'd', 'e']);
+
+        let tree = tree!('a' => { 'b' => {'d'}, 'c' => {'e'} });
+        let mut traverse = tree.bfs();
+        traverse.next();
+        assert!(traverse.peek_at_same_level().is_none());
+        assert_eq!(traverse.nth(2).map(|node| *node.value()), Some('d'));
+        assert_eq!(traverse.peek_at_same_level().map(|node| *node.value()), Some('e'));
     }
 
     #[test]
