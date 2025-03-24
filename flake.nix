938535de2abe3a1e0fcc637ce97d7356c5164b5a
Jason Moggridge <github_hou1@jmoggr.com>
2025-03-24 13:27:41 -0400

    Refactor config into modules and cleanup

--- flake.nix
+++ flake.nix
@@ -2,15 +2,18 @@
   description = "configurations for deploying catcolab";
 
   inputs = {
+    # we currently need unstable to get the correct version of cargo. This should be changed to 25.05
+    # when the next release comes out.
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    agenix.url = "github:ryantm/agenix";
-    deploy-rs.url = "github:serokell/deploy-rs";
 
     # For building rust packages. We need it because the first party `rustPlatform.buildRustPackage` does
     # not work for cargo workspaces: it has a hard requirement that a Cargo.lock exists in the package
     # directory (even though the docs say otherwise). Working around this is possible, but it would
     # require using the whole repository as an input.
     naersk.url = "github:nix-community/naersk";
+
+    agenix.url = "github:ryantm/agenix";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
   outputs =
@@ -40,28 +43,50 @@
       };
     in
     {
-      nixosConfigurations.catcolab-jmoggr = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit catcolabPackages inputs; };
-        system = "x86_64-linux";
-        modules = [
-          ./infrastructure/hosts/catcolab-jmoggr
-          agenix.nixosModules.age
-        ];
+      nixosConfigurations = {
+        catcolab = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit catcolabPackages inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./infrastructure/hosts/catcolab
+            agenix.nixosModules.age
+          ];
+        };
+
+        catcolab-next = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit catcolabPackages inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./infrastructure/hosts/catcolab-next
+            agenix.nixosModules.age
+          ];
+        };
+
+        catcolab-jmoggr = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit catcolabPackages inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./infrastructure/hosts/catcolab-jmoggr
+            agenix.nixosModules.age
+          ];
+        };
       };
 
       deploy.nodes = {
-        # catcolab = {
-        #   hostname = "ec2-18-191-165-64.us-east-2.compute.amazonaws.com";
-        #   profiles.system = mkSystemProfile "catcolab" // {
-        #     sshUser = "root";
-        #   };
-        # };
-        # catcolab-next = {
-        #   hostname = "ec2-18-217-205-18.us-east-2.compute.amazonaws.com";
-        #   profiles.system = mkSystemProfile "catcolab-next" // {
-        #     sshUser = "root";
-        #   };
-        # };
+        catcolab = {
+          hostname = "backend.catcolab.org";
+          profiles.system = {
+            sshUser = "root";
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.catcolab;
+          };
+        };
+        catcolab-next = {
+          hostname = "backend-next.catcolab.org";
+          profiles.system = {
+            sshUser = "root";
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.catcolab-next;
+          };
+        };
         catcolab-jmoggr = {
           hostname = "backend-next.jmoggr.com";
           profiles.system = {
---
+++ infrastructure/modules/backend.nix
@@ -0,0 +1,176 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  config,
+  catcolabPackages,
+  ...
+}:
+
+let
+  # idempotent script for intializing the catcolab database
+  databaseSetupScript = pkgs.writeShellScriptBin "database-setup" ''
+    #!/usr/bin/env bash
+    set -ex
+
+    # Extract the password from the secret file
+    password=$(cat ${config.age.secrets.backendSecretsForPostgres.path} | sed -n 's|.*://[^:]*:\([^@]*\)@.*|\1|p')
+
+    # Create the user only if it doesn't already exist.
+    if ! ${pkgs.postgresql}/bin/psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='catcolab'" | grep -q 1; then
+      ${pkgs.postgresql}/bin/psql -c "CREATE USER catcolab WITH ENCRYPTED PASSWORD '$password';"
+    fi
+
+    # Create the database only if it doesn't already exist.
+    if ! ${pkgs.postgresql}/bin/psql -tAc "SELECT 1 FROM pg_database WHERE datname='catcolab'" | grep -q 1; then
+      ${pkgs.postgresql}/bin/psql -c "CREATE DATABASE catcolab;"
+    fi
+
+    ${pkgs.postgresql}/bin/psql -c "alter database catcolab owner to catcolab;"
+    ${pkgs.postgresql}/bin/psql -c "grant all privileges on database catcolab to catcolab;"
+    ${pkgs.postgresql}/bin/psql -d catcolab -c "grant all on schema public to catcolab;"
+  '';
+
+  databaseMigrationScript = pkgs.writeShellScriptBin "database-migration" ''
+    #!/usr/bin/env bash
+    set -ex
+
+    # the migrations directory is copied into the output of backend
+    cd ${catcolabPackages.backend}
+    ${lib.getExe pkgs.sqlx-cli} migrate run
+  '';
+in
+with lib;
+{
+  options.catcolab.backend = {
+    backendPort = mkOption {
+      type = types.str;
+      default = "8000";
+      description = "Port for the backend service.";
+    };
+    automergePort = mkOption {
+      type = types.str;
+      default = "8010";
+      description = "Port for the automerge service.";
+    };
+    automergeHostname = mkOption {
+      type = types.str;
+      description = "Hostname for the automerge reverse proxy.";
+    };
+    backendHostname = mkOption {
+      type = types.str;
+      description = "Hostname for the backend reverse proxy.";
+    };
+  };
+
+  config = {
+    age.secrets = {
+      backendSecretsForPostgres = {
+        file = "${inputs.self}/infrastructure/secrets/.env.next.age";
+        name = "backend-secrets-for-postgres.env";
+        owner = "postgres";
+      };
+
+      backendSecretsForCatcolab = {
+        file = "${inputs.self}/infrastructure/secrets/.env.next.age";
+        name = "backend-secrets-for-catcolab.env";
+        owner = "catcolab";
+      };
+    };
+
+    services.postgresql = {
+      enable = true;
+    };
+
+    # Database setup and mirgations are run as different services because the database setup requires the
+    # use of the priviledged postgres user and the migrations do not
+    systemd.services.database-setup = {
+      description = "Set up catcolab database and user";
+      after = [ "postgresql.service" ];
+      wants = [ "postgresql.service" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "postgres";
+        ExecStart = lib.getExe databaseSetupScript;
+      };
+    };
+
+    systemd.services.migrations = {
+      enable = true;
+      after = [ "database-setup.service" ];
+      wants = [ "database-setup.service" ];
+
+      serviceConfig = {
+        User = "catcolab";
+        Type = "oneshot";
+        ExecStart = lib.getExe databaseMigrationScript;
+        EnvironmentFile = config.age.secrets.backendSecretsForCatcolab.path;
+      };
+    };
+
+    systemd.services.backend = {
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+      after = [ "migrations.service" ];
+      wants = [ "migrations.service" ];
+
+      environment = {
+        PORT = config.catcolab.backend.backendPort;
+      };
+
+      serviceConfig = {
+        User = "catcolab";
+        Type = "simple";
+        Restart = "on-failure";
+        ExecStart = lib.getExe catcolabPackages.backend;
+        EnvironmentFile = config.age.secrets.backendSecretsForCatcolab.path;
+      };
+    };
+
+    systemd.services.automerge = {
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+
+      environment = {
+        PORT = config.catcolab.backend.automergePort;
+      };
+
+      serviceConfig = {
+        User = "catcolab";
+        ExecStart = "${lib.getExe pkgs.nodejs_23} ${catcolabPackages.automerge-doc-server}/main.cjs";
+        Type = "simple";
+        Restart = "on-failure";
+      };
+    };
+
+    services.caddy = {
+      enable = true;
+      virtualHosts = {
+        "${config.catcolab.backend.backendHostname}" = {
+          extraConfig = ''
+            reverse_proxy :${config.catcolab.backend.backendPort}
+          '';
+        };
+
+        "${config.catcolab.backend.automergeHostname}" = {
+          extraConfig = ''
+            reverse_proxy :${config.catcolab.backend.automergePort}
+          '';
+        };
+      };
+    };
+
+    # install all packages used in this module
+    environment.systemPackages =
+      with pkgs;
+      [
+        nodejs_23
+        sqlx-cli
+      ]
+      ++ [
+        databaseSetupScript
+        databaseMigrationScript
+      ];
+  };
+}
