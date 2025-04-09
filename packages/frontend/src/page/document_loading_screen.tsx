d24f6575776fd1c225f069f9f617981d25f14ef9
Jason Moggridge <github_hou1@jmoggr.com>
2025-04-09 13:54:08 -0400

    Don't flash the 'loading' and show a hamburger menu icon during loading

--- packages/frontend/src/page/document_loading_screen.tsx
+++ packages/frontend/src/page/document_loading_screen.tsx
@@ -1,13 +1,16 @@
+import { HamburgerMenu } from "./menubar";
 import { TheoryHelpButton, Toolbar } from "./toolbar";
 
 export default function DocumentLoadingScreen() {
     return (
         <div class="growable-container">
             <Toolbar>
+                <HamburgerMenu disabled={true}>
+                    <div />
+                </HamburgerMenu>
                 <span class="filler" />
                 <TheoryHelpButton />
             </Toolbar>
-            <div class="notebook-container">Loading...</div>
         </div>
     );
 }
