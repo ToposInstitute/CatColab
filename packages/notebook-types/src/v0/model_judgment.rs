698116c833a762c42882a150b83672617376e80d
Evan Patterson <evan@epatters.org>
2025-05-14 23:23:39 -0700

    REFACTOR: Finish migrating diagram judgment to `notebook-types`.

--- packages/notebook-types/src/v0/model_judgment.rs
+++ packages/notebook-types/src/v0/model_judgment.rs
@@ -5,23 +5,39 @@ use uuid::Uuid;
 use super::model::Ob;
 use super::theory::{MorType, ObType};
 
+/// Declares an object in a model of a double theory.
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi, missing_as_null)]
 pub struct ObDecl {
+    /// Human-readable name of object.
     pub name: String,
+
+    /// Globally unique identifier of object.
     pub id: Uuid,
+
+    /// The object's type in the double theory.
     #[serde(rename = "obType")]
     pub ob_type: ObType,
 }
 
+/// Declares a morphism in a model of a double theory.
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi, missing_as_null)]
 pub struct MorDecl {
+    /// Human-readable name of morphism.
     pub name: String,
+
+    /// Globally unique identifier of morphism.
     pub id: Uuid,
+
+    /// The morphism's type in the double theory.
     #[serde(rename = "morType")]
     pub mor_type: MorType,
+
+    /// Domain of morphism, if defined.
     pub dom: Option<Ob>,
+
+    /// Codomain of morphism, if defined.
     pub cod: Option<Ob>,
 }
 
