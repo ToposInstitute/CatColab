7044314088f0db98ba0ab67946e779210d0b3905
Tim Hosgood <tim@topos.institute>
2025-05-16 19:09:32 +0100

    simple (i.e. cclfo) analysis

---
+++ packages/frontend/src/stdlib/analyses/cclfo.tsx
@@ -0,0 +1,133 @@
+import { createMemo } from "solid-js";
+
+import type {
+    DblModel,
+    CCLFOModelData,
+    CCLFOProblemData,
+    ODEResult,
+} from "catlog-wasm";
+import type { ModelAnalysisProps } from "../../analysis";
+import {
+    type ColumnSchema,
+    FixedTableEditor,
+    Foldable,
+    createNumericalColumn,
+} from "../../components";
+import type { MorphismDecl, ObjectDecl } from "../../model";
+import type { ModelAnalysisMeta } from "../../theory";
+import { ODEResultPlot } from "../../visualization";
+import { createModelODEPlot } from "./simulation";
+
+import "./simulation.css";
+
+/** Configuration for a CCLFO ODE analysis of a model. */
+export type CCLFOContent = CCLFOProblemData<string>;
+
+type Simulator = (model: DblModel, data: CCLFOModelData) => ODEResult;
+
+/** Configure a CCLFO ODE analysis for use with models of a theory. */
+export function configureCCLFO(options: {
+    id?: string;
+    name?: string;
+    description?: string;
+    simulate: Simulator;
+}): ModelAnalysisMeta<CCLFOContent> {
+    const {
+        id = "cclfo",
+        name = "CCLFO dynamics",
+        description = "Simulate the system using a constant-coefficient linear first-order ODE",
+        simulate,
+    } = options;
+    return {
+        id,
+        name,
+        description,
+        component: (props) => <CCLFO simulate={simulate} title={name} {...props} />,
+        initialContent: () => ({
+            interactionCoefficients: {},
+            initialValues: {},
+            duration: 10,
+        }),
+    };
+}
+
+/** Analyze a model using CCLFO dynamics. */
+export function CCLFO(
+    props: ModelAnalysisProps<CCLFOContent> & {
+        simulate: Simulator;
+        title?: string;
+    },
+) {
+    const obDecls = createMemo<ObjectDecl[]>(() => {
+        return props.liveModel.formalJudgments().filter((jgmt) => jgmt.tag === "object");
+    }, []);
+
+    const morDecls = createMemo<MorphismDecl[]>(() => {
+        return props.liveModel.formalJudgments().filter((jgmt) => jgmt.tag === "morphism");
+    }, []);
+
+    const obSchema: ColumnSchema<ObjectDecl>[] = [
+        {
+            contentType: "string",
+            header: true,
+            content: (ob) => ob.name,
+        },
+        createNumericalColumn({
+            name: "Initial value",
+            data: (ob) => props.content.initialValues[ob.id],
+            validate: (_, data) => data >= 0,
+            setData: (ob, data) =>
+                props.changeContent((content) => {
+                    content.initialValues[ob.id] = data;
+                }),
+        }),
+    ];
+
+    const morSchema: ColumnSchema<MorphismDecl>[] = [
+        {
+            contentType: "string",
+            header: true,
+            content: (mor) => mor.name,
+        },
+        createNumericalColumn({
+            name: "Interaction",
+            data: (mor) => props.content.interactionCoefficients[mor.id],
+            default: 1,
+            validate: (_, data) => data >= 0,
+            setData: (mor, data) =>
+                props.changeContent((content) => {
+                    content.interactionCoefficients[mor.id] = data;
+                }),
+        }),
+    ];
+
+    const toplevelSchema: ColumnSchema<null>[] = [
+        createNumericalColumn({
+            name: "Duration",
+            data: (_) => props.content.duration,
+            validate: (_, data) => data >= 0,
+            setData: (_, data) =>
+                props.changeContent((content) => {
+                    content.duration = data;
+                }),
+        }),
+    ];
+
+    const plotResult = createModelODEPlot(
+        () => props.liveModel,
+        (model: DblModel) => props.simulate(model, props.content),
+    );
+
+    return (
+        <div class="simulation">
+            <Foldable title={props.title}>
+                <div class="parameters">
+                    <FixedTableEditor rows={obDecls()} schema={obSchema} />
+                    <FixedTableEditor rows={morDecls()} schema={morSchema} />
+                    <FixedTableEditor rows={[null]} schema={toplevelSchema} />
+                </div>
+            </Foldable>
+            <ODEResultPlot result={plotResult()} />
+        </div>
+    );
+}
