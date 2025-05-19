cf4afbd4c84e797dd6fed20753fae80a12b4c7d3
Jason Moggridge <github_hou1@jmoggr.com>
2025-05-19 14:41:03 -0400

    Rewrite migrations using sqlx_migrator

--- packages/backend/migrations/20250409171833_add_permissions_object_subject_idx.up.sql
+++
@@ -1,5 +0,0 @@
--- no-transaction
-
--- Allow the sorting to be treated as a scan for the `effective_permissions` CTE in document.rs
-CREATE INDEX CONCURRENTLY IF NOT EXISTS permissions_object_subject_idx
-    ON permissions (object, subject);
