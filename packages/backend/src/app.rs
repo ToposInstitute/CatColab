f440020a5e4bc83d5ae650a4233273bc624aebfd
Jason Moggridge <github_hou1@jmoggr.com>
2025-05-22 18:11:28 -0400

    Paginate document search results

--- packages/backend/src/app.rs
+++ packages/backend/src/app.rs
@@ -1,7 +1,9 @@
 use firebase_auth::FirebaseUser;
+use serde::Serialize;
 use socketioxide::SocketIo;
 use sqlx::PgPool;
 use thiserror::Error;
+use ts_rs::TS;
 use uuid::Uuid;
 
 /** Top-level application state.
@@ -25,6 +27,19 @@ pub struct AppCtx {
 
     /// Authenticated Firebase user, if any.
     pub user: Option<FirebaseUser>,
+}
+
+/// A page of items along with pagination metadata.
+#[derive(Clone, Debug, Serialize, TS)]
+pub struct Paginated<T> {
+    /// The total number of items matching the query criteria.
+    pub total: i32,
+
+    /// The number of items skipped.
+    pub offset: i32,
+
+    /// The items in the current page.
+    pub items: Vec<T>,
 }
 
 /// Top-level application error.
