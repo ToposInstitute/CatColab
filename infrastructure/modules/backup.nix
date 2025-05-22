20ef4d1fb2e487b6c1ae702c6282660eaff426fe
Jason <github_hou1@jmoggr.com>
2025-05-22 13:55:07 -0400

    Run backupdb script at activation time to act as a canary (#519)

--- infrastructure/modules/backup.nix
+++ infrastructure/modules/backup.nix
@@ -49,6 +49,32 @@ with lib;
       };
     };
 
+    # run backup script at end of deploy to act as a canary for the backup script
+    system.activationScripts.backupdb = {
+      text = ''
+        echo "Running backupdb script as a transient systemd unit..."
+
+        since=$(date --iso-8601=seconds)
+
+        # we can't run the backupdb unit because at this point systemd doesn't know about the new unit,
+        # so we run a transient unit with the same config.
+        ${pkgs.systemd}/bin/systemd-run --system --wait \
+          --unit=backupdb-activation \
+          --description="One-off activation backupdb" \
+          --property=Type=${config.systemd.services.backupdb.serviceConfig.Type} \
+          --property=User=${config.systemd.services.backupdb.serviceConfig.User} \
+          --property=EnvironmentFile=${config.systemd.services.backupdb.serviceConfig.EnvironmentFile} \
+          --property=Environment=PATH=/run/current-system/sw/bin \
+          ${lib.getExe backupScript}
+
+        exit_code=$?
+        if [ $exit_code -ne 0 ]; then
+          echo "activation‐time backup failed with code $exit_code"
+          ${pkgs.systemd}/bin/journalctl -u backupdb-activation.service --since "$since"
+        fi
+      '';
+    };
+
     # install all packages used in this module
     environment.systemPackages =
       with pkgs;
