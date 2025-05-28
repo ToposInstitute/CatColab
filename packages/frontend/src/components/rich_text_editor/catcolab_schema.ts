c38359c1d68a8642a3e6d2cefa74969ddb77c9dc
Jason Moggridge <github_hou1@jmoggr.com>
2025-05-28 15:36:36 -0400

    Add prosemirror KaTex plugin and menu bar for RTE

---
+++ packages/frontend/src/components/rich_text_editor/catcolab_schema.ts
@@ -0,0 +1,6 @@
+import type { MappedSchemaSpec } from "@automerge/prosemirror";
+
+// This is where we would specify any "custom" schemas
+export const catcolabSchema = {
+    nodes: {},
+} satisfies MappedSchemaSpec;
