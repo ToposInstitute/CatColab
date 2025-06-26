ef31a65510c61e9cb1ebb04e53b8194c32082ed1
Tim Hosgood <tim@topos.institute>
2025-06-26 16:21:57 +0100

    type

--- packages/catlog/src/stdlib/analyses/ecld/normal_form.rs
+++ packages/catlog/src/stdlib/analyses/ecld/normal_form.rs
@@ -1,13 +1,13 @@
 //! Transforming any ECLD into its "normal form", i.e. replacing length-n paths
 //  with n length-1 paths.
 
+use crate::dbl::model::DiscreteDblModel;
+use crate::one::fp_category::UstrFpCategory;
 use std::rc::Rc;
 use crate::dbl::model::UstrDiscreteDblModel;
-use crate::dbl::theory::*;
 
 /** Given an ECLD, return a new ECLD of its normal form
  */
-pub fn normal_form(th: Rc<UstrDiscreteDblTheory>) -> UstrDiscreteDblModel {
-    let n_model = UstrDiscreteDblModel::new(th);
-    n_model
+pub fn normal_form<Uuid>(_th: Rc<DiscreteDblModel<Uuid, UstrFpCategory>>) -> UstrDiscreteDblModel {
+    panic!("oops");
 }
