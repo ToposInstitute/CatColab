b978ab7ac82a006dcce2d3a9b4d74adf511c4bff
Matt Cuffaro <cuffaro.matt@gmail.com>
2025-07-18 11:34:06 -0700

    ENH: moved login into overlay; no notebook diff

--- packages/frontend/src/model/welcome_overlay.tsx
+++ packages/frontend/src/model/welcome_overlay.tsx
@@ -1,9 +1,8 @@
 import { Binoculars, Bird, Github, NotebookPen } from "lucide-solid";
-import { createSignal } from "solid-js";
+import { Match, Switch, createSignal } from "solid-js";
 import zulip_favicon from "./assets/zulip_favicon.png";
 import "./welcome_overlay.css";
 
-import { Dialog } from "../components";
 import { Login } from "../user";
 
 export function WelcomeOverlay(props: { isOpen: boolean; onClose: () => void }) {
@@ -23,82 +22,100 @@ export function WelcomeOverlay(props: { isOpen: boolean; onClose: () => void }) 
             aria-modal="true"
         >
             <div class="welcome-content" onClick={(e) => e.stopPropagation()}>
-                <div class="header-container">
+                <div class="welcome-header-container">
                     <img
                         src="https://topos.institute/assets/logo-name.png"
                         alt="Topos Institute"
                         class="topos-logo"
                     />
                 </div>
-
-                <div class="welcome-body fade-in">
-                    <h2>Welcome to CatColab!</h2>
-                    <i>software for modeling the world, together</i>
-                    <div class="resources-container">
-                        <div class="resources-list">
-                            <a
-                                href="https://catcolab.org/help"
-                                class="resource-link"
-                                target="_blank"
-                            >
-                                <span class="resource-icon">
-                                    <Bird />
-                                </span>
-                                <span>CatColab Overview</span>
-                            </a>
-                            <a
-                                href="https://catcolab.org/help/quick-intro"
-                                class="resource-link"
-                                target="_blank"
-                            >
-                                <span class="resource-icon">
-                                    <Binoculars />
-                                </span>
-                                <span>Example Models</span>
-                            </a>
-                            <a
-                                href="https://topos.institute/blog/#category=CatColab"
-                                class="resource-link"
-                                target="_blank"
-                            >
-                                <span class="resource-icon">
-                                    <NotebookPen />
-                                </span>
-                                <span>Blog Post</span>
-                            </a>
-                            <a
-                                href="https://github.com/ToposInstitute/CatColab"
-                                class="resource-link"
-                                target="_blank"
-                            >
-                                <span class="resource-icon">
-                                    <Github />
-                                </span>
-                                <span>Source Code</span>
-                            </a>
-                            <a href="" class="resource-link" target="_blank">
-                                <span class="resource-icon">
-                                    <img src={zulip_favicon} width="24" height="24" />
-                                </span>
-                                <span>Join us on Zulip!</span>
-                            </a>
+                <Switch>
+                    <Match when={loginOpen()}>
+                        <div class="welcome-body fade-in">
+                            <Login onComplete={handleLoginComplete} />
+                            <div class="welcome-navigation-buttons left">
+                                <button
+                                    class="welcome-nav-button"
+                                    onClick={() => setLoginOpen(false)}
+                                >
+                                    Go back
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-                <div class="footer-container">
-                    <div class="navigation-buttons">
-                        <button class="nav-button get-started" onClick={() => setLoginOpen(true)}>
-                            Login/Sign-Up
-                        </button>
-                        <button class="nav-button get-started" onClick={props.onClose}>
-                            Get Started
-                        </button>
-                    </div>
-                </div>
-                <Dialog open={loginOpen()} onOpenChange={setLoginOpen} title="Log in">
-                    <Login onComplete={handleLoginComplete} />
-                </Dialog>
+                    </Match>
+                    <Match when={!loginOpen()}>
+                        <div class="welcome-body fade-in">
+                            <h2>Welcome to CatColab!</h2>
+                            <i>software for modeling the world, together</i>
+                            <div class="resources-container">
+                                <div class="resources-list">
+                                    <a
+                                        href="https://catcolab.org/help"
+                                        class="resource-link"
+                                        target="_blank"
+                                    >
+                                        <span class="resource-icon">
+                                            <Bird />
+                                        </span>
+                                        <span>CatColab Overview</span>
+                                    </a>
+                                    <a
+                                        href="https://catcolab.org/help/quick-intro"
+                                        class="resource-link"
+                                        target="_blank"
+                                    >
+                                        <span class="resource-icon">
+                                            <Binoculars />
+                                        </span>
+                                        <span>Example Models</span>
+                                    </a>
+                                    <a
+                                        href="https://topos.institute/blog/#category=CatColab"
+                                        class="resource-link"
+                                        target="_blank"
+                                    >
+                                        <span class="resource-icon">
+                                            <NotebookPen />
+                                        </span>
+                                        <span>Blog Post</span>
+                                    </a>
+                                    <a
+                                        href="https://github.com/ToposInstitute/CatColab"
+                                        class="resource-link"
+                                        target="_blank"
+                                    >
+                                        <span class="resource-icon">
+                                            <Github />
+                                        </span>
+                                        <span>Source Code</span>
+                                    </a>
+                                    <a href="" class="resource-link" target="_blank">
+                                        <span class="resource-icon">
+                                            <img src={zulip_favicon} width="24" height="24" />
+                                        </span>
+                                        <span>Join us on Zulip!</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="welcome-footer-container">
+                            <div class="welcome-navigation-buttons">
+                                <button
+                                    class="welcome-nav-button get-started"
+                                    onClick={() => setLoginOpen(true)}
+                                >
+                                    Login/Sign-Up
+                                </button>
+                                <button
+                                    class="welcome-nav-button get-started"
+                                    onClick={props.onClose}
+                                >
+                                    Get Started
+                                </button>
+                            </div>
+                        </div>
+                    </Match>
+                </Switch>
             </div>
         </div>
     );
