0076214b39a8acb4831314e69379f0fb103b2016
Tim Hosgood <tim@topos.institute>
2025-07-25 00:03:01 +0100

    created files

--- packages/frontend/src/stdlib/theories/empty.ts
+++ packages/frontend/src/stdlib/theories/empty.ts
@@ -1,12 +1,13 @@
 import * as catlog from "catlog-wasm";
 
 import { Theory } from "../../theory";
-import { TheoryMeta } from "../types";
+import type { TheoryMeta } from "../types";
 
-export function createEmptyTheory(theoryMeta: TheoryMeta): Theory {
-  const thEmpty = new catlog.ThEmpty();
-  return new Theory({
-    ...theoryMeta,
-    theory: thEmpty.theory(),
-  });
+export default function createEmptyTheory(theoryMeta: TheoryMeta): Theory {
+    const thEmpty = new catlog.ThEmpty();
+
+    return new Theory({
+        ...theoryMeta,
+        theory: thEmpty.theory(),
+    });
 }
