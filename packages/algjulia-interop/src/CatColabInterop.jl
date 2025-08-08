823f187b8da6fd5f64e51fddcac1fae554d144d2
Matt Cuffaro <cuffaro.matt@gmail.com>
2025-08-07 18:10:48 -0700

    ENH: moving jupyter_server script to main

--- packages/algjulia-interop/src/CatColabInterop.jl
+++ packages/algjulia-interop/src/CatColabInterop.jl
@@ -76,6 +76,7 @@ export ImplError
 
 Base.showerror(io::IO, e::ImplError) = print(io, "$(e.name) not implemented")
 
+include("run_jupyter.jl")
 include("kernel_support.jl")
 include("decapodes-service/DecapodesService.jl")
 
