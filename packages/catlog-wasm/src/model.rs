c140f25aefd725d8d593570e794087d08238b67c
Evan Patterson <evan@epatters.org>
2025-10-16 20:44:52 -0700

    ENH: Extend notebook types to allow instantiating models in models.

--- packages/catlog-wasm/src/model.rs
+++ packages/catlog-wasm/src/model.rs
@@ -541,6 +541,7 @@ pub fn elaborate_model(notebook: ModelNotebook, theory: &DblTheory) -> Result<Db
         match judgment {
             ModelJudgment::Object(decl) => model.add_ob(decl)?,
             ModelJudgment::Morphism(decl) => model.add_mor(decl)?,
+            ModelJudgment::Instantiation(_) => {} // FIXME: Ignored for now.
         }
     }
     Ok(model)
