89f9227d539d6adb0554ef1a24f335106e69533e
Kaspar Bumke <git@kaspar.systems>
2025-11-03 11:21:57 +0000

    TEST: Setup storybook vitest for ui-components

--- packages/ui-components/vite.config.ts
+++ packages/ui-components/vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
                             },
                         ],
                     },
+                    setupFiles: ["./.storybook/vitest.setup.ts"],
                 },
             },
         ],
