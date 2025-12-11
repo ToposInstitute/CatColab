f96f72caca59d994f8435d00a480ba7e6dd8c9b7
Kaspar Bumke <git@kaspar.systems>
2025-12-11 11:38:21 +0000

    REFACTOR: Purge kobalte by re-implementing alert

--- packages/ui-components/src/alert.tsx
+++ packages/ui-components/src/alert.tsx
@@ -1,4 +1,3 @@
-import { Alert } from "@kobalte/core/alert";
 import Info from "lucide-solid/icons/info";
 import MessageCircleQuestion from "lucide-solid/icons/message-circle-question";
 import OctagonX from "lucide-solid/icons/octagon-x";
@@ -17,46 +16,46 @@ export type AlertProps = {
 
 /** A warning alert. */
 export const Warning = (props: AlertProps) => (
-    <Alert class="alert alert-warning">
+    <div role="alert" class="alert alert-warning">
         <div class="alert-heading">
             <TriangleAlert />
             {props.title ?? "Warning"}
         </div>
         {props.children}
-    </Alert>
+    </div>
 );
 
 /** An error alert.
 Not called `Error` to avoid shadowing that name in JavaScript.
  */
 export const ErrorAlert = (props: AlertProps) => (
-    <Alert class="alert alert-error">
+    <div role="alert" class="alert alert-error">
         <div class="alert-heading">
             <OctagonX />
             {props.title ?? "Error"}
         </div>
         {props.children}
-    </Alert>
+    </div>
 );
 
 /** A note alert. */
 export const Note = (props: AlertProps) => (
-    <Alert class="alert alert-note">
+    <div role="alert" class="alert alert-note">
         <div class="alert-heading">
             <Info />
             {props.title ?? "Note"}
         </div>
         {props.children}
-    </Alert>
+    </div>
 );
 
 /** A question alert. */
 export const Question = (props: AlertProps) => (
-    <Alert class="alert alert-question">
+    <div role="alert" class="alert alert-question">
         <div class="alert-heading">
             <MessageCircleQuestion />
             {props.title ?? "Question"}
         </div>
         {props.children}
-    </Alert>
+    </div>
 );
