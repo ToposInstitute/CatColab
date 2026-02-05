5a06dc5f9c1470db35d9be9a0829b73cc81857fb
George Thomas <georgefsthomas@gmail.com>
2026-02-05 11:41:16 +0000

    Remove Lotka-Voltera type

--- packages/catlog/src/simulate/ode/lotka_volterra.rs
+++ packages/catlog/src/simulate/ode/lotka_volterra.rs
@@ -6,52 +6,31 @@ use nalgebra::{DMatrix, DVector};
 
 #[cfg(test)]
 use super::ODEProblem;
-use super::ODESystem;
 
-/// A Lotka-Volterra dynamical system.
+/// Construct a Lotka-Volterra dynamical system.
 ///
 /// A system of ODEs that is affine in its *logarithmic* derivative. These are
 /// sometimes called the "generalized Lotka-Volterra equations." For more, see
 /// [Wikipedia](https://en.wikipedia.org/wiki/Generalized_Lotka%E2%80%93Volterra_equation).
-#[derive(Clone, Debug, PartialEq)]
-pub struct LotkaVolterraSystem {
+pub fn lotka_volterra_system(
     interaction_coeffs: DMatrix<f32>,
     growth_rates: DVector<f32>,
-}
-
-impl LotkaVolterraSystem {
-    /// Constructs a new Lokta-Volterra system with the given parameters.
-    pub fn new(A: DMatrix<f32>, b: DVector<f32>) -> Self {
-        Self { interaction_coeffs: A, growth_rates: b }
-    }
-
-    /// Converts to a numerical polynomial system.
-    pub fn to_polynomial(self) -> NumericalPolynomialSystem<u8> {
-        NumericalPolynomialSystem {
-            components: self
-                .interaction_coeffs
-                .row_iter()
-                .enumerate()
-                .zip(self.growth_rates.into_iter())
-                .map(|((i, row), rate)| {
-                    Polynomial::<_, f32, _>::generator(i)
-                        * (row
-                            .iter()
-                            .enumerate()
-                            .map(|(j, a)| Polynomial::generator(j) * *a)
-                            .sum::<Polynomial<_, _, _>>()
-                            + *rate)
-                })
-                .collect(),
-        }
-    }
-}
-
-impl ODESystem for LotkaVolterraSystem {
-    fn vector_field(&self, dx: &mut DVector<f32>, x: &DVector<f32>, _t: f32) {
-        let A = &self.interaction_coeffs;
-        let b = &self.growth_rates;
-        *dx = (A * x + b).component_mul(x);
+) -> NumericalPolynomialSystem<u8> {
+    NumericalPolynomialSystem {
+        components: interaction_coeffs
+            .row_iter()
+            .enumerate()
+            .zip(growth_rates.into_iter())
+            .map(|((i, row), rate)| {
+                Polynomial::<_, f32, _>::generator(i)
+                    * (row
+                        .iter()
+                        .enumerate()
+                        .map(|(j, a)| Polynomial::generator(j) * *a)
+                        .sum::<Polynomial<_, _, _>>()
+                        + *rate)
+            })
+            .collect(),
     }
 }
 
@@ -59,10 +38,8 @@ impl ODESystem for LotkaVolterraSystem {
 pub(crate) fn create_predator_prey() -> ODEProblem<NumericalPolynomialSystem<u8>> {
     let A = DMatrix::from_row_slice(2, 2, &[0.0, -1.0, 1.0, 0.0]);
     let b = DVector::from_column_slice(&[2.0, -1.0]);
-    let sys = LotkaVolterraSystem::new(A, b).to_polynomial();
-
     let x0 = DVector::from_column_slice(&[1.0, 1.0]);
-    ODEProblem::new(sys, x0).end_time(10.0)
+    ODEProblem::new(lotka_volterra_system(A, b), x0).end_time(10.0)
 }
 
 #[cfg(test)]
