ToposInstitute/CatColab

commit ecaf3a34422bb73de6c23dd26152bf573e32633d

REFACTOR: Separate out `TmV`s that can extracted to objects.

packages/catlog/src/tt/eval.rs
@@ -84,7 +84,9 @@ impl<'a> Evaluator<'a> {
             TmS_::Tt => TmV::Tt,
             TmS_::Id(_) => TmV::Opaque,
             TmS_::Compose(_, _) => TmV::Opaque,
-            TmS_::List(elems) => TmV::list(elems.iter().map(|tm| self.eval_tm(tm)).collect()),
+            TmS_::List(elems) => {
+                TmV::list(elems.iter().map(|tm| self.eval_tm(tm).unwrap_ob()).collect())
+            }
             TmS_::Opaque => TmV::Opaque,
             TmS_::Meta(mv) => TmV::Meta(*mv),
         }
@@ -93,7 +95,7 @@ impl<'a> Evaluator<'a> {
     /// Compute the projection of a field from a term value
     pub fn proj(&self, tm: &TmV, field_name: FieldName, field_label: LabelSegment) -> TmV {
         match tm {
-            TmV::Neu(n, ty) => TmV::Neu(
+            TmV::Ob(ObTmV::Neu(n, ty)) => TmV::neu(
                 TmN::proj(n.clone(), field_name, field_label),
                 self.field_ty(ty, tm, field_name),
             ),
@@ -121,7 +123,7 @@ impl<'a> Evaluator<'a> {
     /// Bind a new neutral of type `ty`
     pub fn bind_neu(&self, name: VarName, label: LabelSegment, ty: TyV) -> (TmN, Self) {
         let n = TmN::var(self.scope_length.into(), name, label);
-        let v = TmV::Neu(n.clone(), ty);
+        let v = TmV::neu(n.clone(), ty);
         (
             n,
             Self {
@@ -186,7 +188,7 @@ impl<'a> Evaluator<'a> {
         }
     }
 
-    /// Produce term syntax from a term neutral.
+    /// Produce term syntax from a neutral term.
     ///
     /// The documentation for [Evaluator::quote_ty] is also applicable here.
     pub fn quote_neu(&self, n: &TmN) -> TmS {
@@ -196,14 +198,21 @@ impl<'a> Evaluator<'a> {
         }
     }
 
+    /// Produce term syntax from an object term.
+    fn quote_ob(&self, tm: &ObTmV) -> TmS {
+        match tm {
+            ObTmV::Neu(n, _) => self.quote_neu(n),
+            ObTmV::List(elems) => TmS::list(elems.iter().map(|ob| self.quote_ob(ob)).collect()),
+        }
+    }
+
     /// Produce term syntax from a term value.
     ///
     /// The documentation for [Evaluator::quote_ty] is also applicable here.
     pub fn quote_tm(&self, tm: &TmV) -> TmS {
         match tm {
-            TmV::Neu(n, _) => self.quote_neu(n),
+            TmV::Ob(ob) => self.quote_ob(ob),
             TmV::Cons(fields) => TmS::cons(fields.map(|tm| self.quote_tm(tm))),
-            TmV::List(elems) => TmS::list(elems.iter().map(|tm| self.quote_tm(tm)).collect()),
             TmV::Tt => TmS::tt(),
             TmV::Opaque => TmS::opaque(),
             TmV::Meta(mv) => TmS::meta(*mv),
@@ -279,7 +288,7 @@ impl<'a> Evaluator<'a> {
                     self1.convertable_ty(&field_ty1_v, &field_ty2_v)?;
                     let (field_val, self_next) = self.bind_neu(*name, *label, field_ty1_v.clone());
                     self1 = self_next;
-                    fields.insert(*name, (*label, TmV::Neu(field_val, field_ty1_v)));
+                    fields.insert(*name, (*label, TmV::neu(field_val, field_ty1_v)));
                 }
                 Ok(())
             }
@@ -293,7 +302,7 @@ impl<'a> Evaluator<'a> {
     /// Performs eta-expansion of the neutral `n` at type `ty`.
     pub fn eta_neu(&self, n: &TmN, ty: &TyV) -> TmV {
         match &**ty {
-            TyV_::Object(_) => TmV::Neu(n.clone(), ty.clone()),
+            TyV_::Object(_) => TmV::neu(n.clone(), ty.clone()),
             TyV_::Morphism(_, _, _) => TmV::Opaque,
             TyV_::Record(r) => {
                 let mut fields = Row::empty();
@@ -306,14 +315,24 @@ impl<'a> Evaluator<'a> {
             }
             TyV_::Sing(_, x) => x.clone(),
             TyV_::Unit => TmV::Tt,
-            TyV_::Meta(_) => TmV::Neu(n.clone(), ty.clone()),
+            TyV_::Meta(_) => TmV::neu(n.clone(), ty.clone()),
+        }
+    }
+
+    /// Performs eta-expansion of an object term value at type `ty`.
+    fn eta_ob(&self, ob: &ObTmV) -> TmV {
+        match ob {
+            ObTmV::Neu(tm_n, ty_v) => self.eta_neu(tm_n, ty_v),
+            ObTmV::List(elems) => {
+                TmV::list(elems.iter().map(|elem| self.eta_ob(elem).unwrap_ob()).collect())
+            }
         }
     }
 
     /// Performs eta-expansion of the term `n` at type `ty`
     pub fn eta(&self, v: &TmV, ty: &TyV) -> TmV {
         match v {
-            TmV::Neu(tm_n, ty_v) => self.eta_neu(tm_n, ty_v),
+            TmV::Ob(ob) => self.eta_ob(ob),
             TmV::Cons(row) => TmV::Cons(
                 row.iter()
                     .map(|(name, (label, field_v))| {
@@ -321,16 +340,6 @@ impl<'a> Evaluator<'a> {
                     })
                     .collect(),
             ),
-            TmV::List(elems) => {
-                let TyV_::Object(ob_type) = &**ty else {
-                    panic!("Type of list should be object type");
-                };
-                let Some(ob_type) = ob_type.clone().list_arg() else {
-                    panic!("Type of list should be application of list modality");
-                };
-                let ty = TyV::object(ob_type);
-                TmV::list(elems.iter().map(|elem| self.eta(elem, &ty)).collect())
-            }
             TmV::Tt => TmV::Tt,
             TmV::Opaque => TmV::Opaque,
             TmV::Meta(_) => v.clone(),
@@ -360,13 +369,13 @@ impl<'a> Evaluator<'a> {
         strict2: bool,
     ) -> Result<(), D<'b>> {
         match (tm1, tm2) {
-            (TmV::Neu(n1, ty1), _) if !strict1 => {
+            (TmV::Ob(ObTmV::Neu(n1, ty1)), _) if !strict1 => {
                 self.equal_tm_helper(&self.eta_neu(n1, ty1), tm2, true, strict2)
             }
-            (_, TmV::Neu(n2, ty2)) if !strict2 => {
+            (_, TmV::Ob(ObTmV::Neu(n2, ty2))) if !strict2 => {
                 self.equal_tm_helper(tm1, &self.eta_neu(n2, ty2), strict1, true)
             }
-            (TmV::Neu(n1, _), TmV::Neu(n2, _)) => {
+            (TmV::Ob(ObTmV::Neu(n1, _)), TmV::Ob(ObTmV::Neu(n2, _))) => {
                 if n1 == n2 {
                     Ok(())
                 } else {

packages/catlog/src/tt/modelgen.rs
@@ -76,10 +76,18 @@ impl Model {
         });
     }
 
-    /// Makes an object from a term.
-    fn make_ob(&self, val: &TmV) -> Option<Ob> {
+    /// Tries to make an object from a term.
+    fn maybe_make_ob(&self, val: &TmV) -> Option<Ob> {
         match val {
-            TmV::Neu(n, _) => {
+            TmV::Ob(ob) => Some(self.make_ob(ob)),
+            _ => None,
+        }
+    }
+
+    /// Makes an object from an object term.
+    fn make_ob(&self, b: &ObTmV) -> Ob {
+        match b {
+            ObTmV::Neu(n, _) => {
                 let mut segments = Vec::new();
                 let mut n = n.clone();
                 while let TmN_::Proj(n1, f, _) = &*n.clone() {
@@ -87,13 +95,11 @@ impl Model {
                     segments.push(*f);
                 }
                 segments.reverse();
-                Some(self.ob_generator(segments.into()))
+                self.ob_generator(segments.into())
             }
-            TmV::List(elems) => {
-                let elems: Option<Vec<_>> = elems.iter().map(|val| self.make_ob(val)).collect();
-                elems.and_then(|elems| self.ob_list(elems))
-            }
-            _ => None,
+            ObTmV::List(elems) => self
+                .ob_list(elems.iter().map(|tm| self.make_ob(tm)).collect())
+                .expect("theory should support lists"),
         }
     }
 
@@ -154,7 +160,8 @@ impl<'a> ModelGenerator<'a> {
                 None
             }
             TyV_::Morphism(mt, dom, cod) => {
-                let (dom, cod) = (self.output.make_ob(dom)?, self.output.make_ob(cod)?);
+                let dom = self.output.maybe_make_ob(dom)?;
+                let cod = self.output.maybe_make_ob(cod)?;
                 self.output.add_mor(prefix.into(), dom, cod, mt.clone());
                 None
             }

packages/catlog/src/tt/notebook_elab.rs
@@ -81,7 +81,7 @@ impl<'a> Elaborator<'a> {
     }
 
     fn intro(&mut self, name: VarName, label: LabelSegment, ty: Option<TyV>) -> TmV {
-        let v = TmV::Neu(
+        let v = TmV::neu(
             TmN::var(self.ctx.scope.len().into(), name, label),
             ty.clone().unwrap_or(TyV::unit()),
         );
@@ -318,7 +318,7 @@ impl<'a> Elaborator<'a> {
             field_ty_vs.push((name, (label, ty_v.clone())));
             self.ctx.scope.push(VarInContext::new(name, label, Some(ty_v.clone())));
             self.ctx.env =
-                self.ctx.env.snoc(TmV::Neu(TmN::proj(self_var.clone(), name, label), ty_v));
+                self.ctx.env.snoc(TmV::neu(TmN::proj(self_var.clone(), name, label), ty_v));
         }
 
         self.reset_to(c);

packages/catlog/src/tt/text_elab.rs
@@ -316,7 +316,7 @@ impl<'a> Elaborator<'a> {
     }
 
     fn intro(&mut self, name: VarName, label: LabelSegment, ty: Option<TyV>) -> TmV {
-        let v = TmV::Neu(
+        let v = TmV::neu(
             TmN::var(self.ctx.scope.len().into(), name, label),
             ty.clone().unwrap_or(TyV::unit()),
         );
@@ -463,7 +463,7 @@ impl<'a> Elaborator<'a> {
                     field_ty_vs.push((name, (label, ty_v.clone())));
                     elab.ctx.push_scope(name, label, Some(ty_v.clone()));
                     elab.ctx.env =
-                        elab.ctx.env.snoc(TmV::Neu(TmN::proj(self_var.clone(), name, label), ty_v));
+                        elab.ctx.env.snoc(TmV::neu(TmN::proj(self_var.clone(), name, label), ty_v));
                 }
                 if failed {
                     return elab.ty_hole();
@@ -668,7 +668,7 @@ impl<'a> Elaborator<'a> {
                     elab.loc = Some(ob_n.loc());
                     let (tm_s, tm_v) = elab.chk(&elem_ty_v, ob_n);
                     elem_stxs.push(tm_s);
-                    elem_vals.push(tm_v);
+                    elem_vals.push(tm_v.unwrap_ob());
                 }
                 (TmS::list(elem_stxs), TmV::list(elem_vals))
             }

packages/catlog/src/tt/val.rs
@@ -190,25 +190,44 @@ impl TmN {
     }
 }
 
+/// Values for terms of object type.
+///
+/// These are terms that can be potentially extracted to an object in a model.
+/// That is not possible for a generic [`TmV`]. This way of structuring values
+/// also ensures that it is impossible to construct, say, a list of records;
+/// only lists of objects are allowed.
+#[derive(Clone)]
+pub enum ObTmV {
+    /// Neutrals.
+    ///
+    /// We store the type because we need it for eta-expansion.
+    Neu(TmN, TyV),
+    /// Lists of objects.
+    List(Rc<Vec<ObTmV>>),
+}
+
+impl ObTmV {
+    /// Smart constructor for lists.
+    pub fn list(elems: Vec<ObTmV>) -> Self {
+        ObTmV::List(Rc::new(elems))
+    }
+}
+
 /// Values for terms in the codiscrete mode.
 ///
 /// Note that this is *not* the value for a general term. So evaluating a `TmS`
 /// to produce a `TmV` and then quoting back will lose information about
 /// anything morphism-related. See [crate::tt] for more information.
 ///
-/// It turns out that each of the cases for [TmV] has a single cheaply cloneable
-/// field, so we don't need to bother making a `TmV_`.
+/// Each of the cases for [TmV] has a single cheaply cloneable field, so we
+/// don't need to bother making an inner type `TmV_`.
 #[derive(Clone)]
 pub enum TmV {
-    /// Neutrals.
-    ///
-    /// We store the type because we need it for eta-expansion.
-    Neu(TmN, TyV),
-    /// Lists.
-    List(Rc<Vec<TmV>>),
+    /// Object terms.
+    Ob(ObTmV),
     /// Records.
     Cons(Row<TmV>),
-    /// The unique element of `Ty0::Unit`.
+    /// The unique element of the unit type.
     Tt,
     /// An element of a type that is opaque to conversion checking
     Opaque,
@@ -217,15 +236,28 @@ pub enum TmV {
 }
 
 impl TmV {
-    /// Smart constructor for [`TmV`], [`TmV::List`] variant.
-    pub fn list(elems: Vec<TmV>) -> Self {
-        TmV::List(Rc::new(elems))
+    /// Smart constructor for neutrals.
+    pub fn neu(n: TmN, ty: TyV) -> Self {
+        TmV::Ob(ObTmV::Neu(n, ty))
+    }
+
+    /// Smart constructor for lists.
+    pub fn list(elems: Vec<ObTmV>) -> Self {
+        TmV::Ob(ObTmV::list(elems))
+    }
+
+    /// Unwraps an object term, or panics.
+    pub fn unwrap_ob(&self) -> ObTmV {
+        match self {
+            TmV::Ob(ob) => ob.clone(),
+            _ => panic!("expected term to be an object"),
+        }
     }
 
     /// Unwraps a neutral term, or panics.
     pub fn unwrap_neu(&self) -> TmN {
         match self {
-            TmV::Neu(n, _) => n.clone(),
+            TmV::Ob(ObTmV::Neu(n, _)) => n.clone(),
             _ => panic!("expected term to be a neutral"),
         }
     }

commit 2545b3503f32958ab4c53a114553534742f8b117

REFACTOR: Collapse inner enum `ObTmV` back into `TmV`.
This commit reverts the refactor in ecaf3a3, which caused more problems
that it solved. Specifically, it failed to account for the fact that,
when elaboration encounters errors, metavariables (holes) can appear
where object terms would otherwise be. To work around the original
problem, the `eta` method now makes the `ty` argument optional, as it is
not needed for object terms.

packages/catlog/src/tt/eval.rs
@@ -81,10 +81,8 @@ impl<'a> Evaluator<'a> {
             TmS_::Tt => TmV::Tt,
             TmS_::Id(_) => TmV::Opaque,
             TmS_::Compose(_, _) => TmV::Opaque,
-            TmS_::ObApp(name, x) => TmV::app(*name, self.eval_tm(x).unwrap_ob()),
-            TmS_::List(elems) => {
-                TmV::list(elems.iter().map(|tm| self.eval_tm(tm).unwrap_ob()).collect())
-            }
+            TmS_::ObApp(name, x) => TmV::app(*name, self.eval_tm(x)),
+            TmS_::List(elems) => TmV::list(elems.iter().map(|tm| self.eval_tm(tm)).collect()),
             TmS_::Opaque => TmV::Opaque,
             TmS_::Meta(mv) => TmV::Meta(*mv),
         }
@@ -93,7 +91,7 @@ impl<'a> Evaluator<'a> {
     /// Compute the projection of a field from a term value
     pub fn proj(&self, tm: &TmV, field_name: FieldName, field_label: LabelSegment) -> TmV {
         match tm {
-            TmV::Ob(ObTmV::Neu(n, ty)) => TmV::neu(
+            TmV::Neu(n, ty) => TmV::neu(
                 TmN::proj(n.clone(), field_name, field_label),
                 self.field_ty(ty, tm, field_name),
             ),
@@ -196,21 +194,14 @@ impl<'a> Evaluator<'a> {
         }
     }
 
-    /// Produce term syntax from an object term.
-    fn quote_ob(&self, tm: &ObTmV) -> TmS {
-        match tm {
-            ObTmV::Neu(n, _) => self.quote_neu(n),
-            ObTmV::App(name, x) => TmS::ob_app(*name, self.quote_ob(x)),
-            ObTmV::List(elems) => TmS::list(elems.iter().map(|ob| self.quote_ob(ob)).collect()),
-        }
-    }
-
     /// Produce term syntax from a term value.
     ///
     /// The documentation for [Evaluator::quote_ty] is also applicable here.
     pub fn quote_tm(&self, tm: &TmV) -> TmS {
         match tm {
-            TmV::Ob(ob) => self.quote_ob(ob),
+            TmV::Neu(n, _) => self.quote_neu(n),
+            TmV::App(name, x) => TmS::ob_app(*name, self.quote_tm(x)),
+            TmV::List(elems) => TmS::list(elems.iter().map(|tm| self.quote_tm(tm)).collect()),
             TmV::Cons(fields) => TmS::cons(fields.map(|tm| self.quote_tm(tm))),
             TmV::Tt => TmS::tt(),
             TmV::Opaque => TmS::opaque(),
@@ -318,28 +309,25 @@ impl<'a> Evaluator<'a> {
         }
     }
 
-    /// Performs eta-expansion of an object term value at type `ty`.
-    fn eta_ob(&self, ob: &ObTmV) -> TmV {
-        match ob {
-            ObTmV::Neu(tm_n, ty_v) => self.eta_neu(tm_n, ty_v),
-            ObTmV::App(name, ob) => TmV::app(*name, self.eta_ob(ob).unwrap_ob()),
-            ObTmV::List(elems) => {
-                TmV::list(elems.iter().map(|elem| self.eta_ob(elem).unwrap_ob()).collect())
-            }
-        }
-    }
-
     /// Performs eta-expansion of the term `n` at type `ty`
-    pub fn eta(&self, v: &TmV, ty: &TyV) -> TmV {
+    pub fn eta(&self, v: &TmV, ty: Option<&TyV>) -> TmV {
         match v {
-            TmV::Ob(ob) => self.eta_ob(ob),
-            TmV::Cons(row) => TmV::Cons(
-                row.iter()
-                    .map(|(name, (label, field_v))| {
-                        (*name, (*label, self.eta(field_v, &self.field_ty(ty, v, *name))))
-                    })
-                    .collect(),
-            ),
+            TmV::Neu(tm_n, ty_v) => self.eta_neu(tm_n, ty_v),
+            TmV::App(name, x) => TmV::app(*name, self.eta(x, None)),
+            TmV::List(elems) => TmV::list(elems.iter().map(|elem| self.eta(elem, None)).collect()),
+            TmV::Cons(row) => {
+                if let Some(ty) = ty {
+                    let row = row
+                        .iter()
+                        .map(|(name, (label, field_v))| {
+                            (*name, (*label, self.eta(field_v, Some(&self.field_ty(ty, v, *name)))))
+                        })
+                        .collect();
+                    TmV::Cons(row)
+                } else {
+                    v.clone()
+                }
+            }
             TmV::Tt => TmV::Tt,
             TmV::Opaque => TmV::Opaque,
             TmV::Meta(_) => v.clone(),
@@ -369,13 +357,13 @@ impl<'a> Evaluator<'a> {
         strict2: bool,
     ) -> Result<(), D<'b>> {
         match (tm1, tm2) {
-            (TmV::Ob(ObTmV::Neu(n1, ty1)), _) if !strict1 => {
+            (TmV::Neu(n1, ty1), _) if !strict1 => {
                 self.equal_tm_helper(&self.eta_neu(n1, ty1), tm2, true, strict2)
             }
-            (_, TmV::Ob(ObTmV::Neu(n2, ty2))) if !strict2 => {
+            (_, TmV::Neu(n2, ty2)) if !strict2 => {
                 self.equal_tm_helper(tm1, &self.eta_neu(n2, ty2), strict1, true)
             }
-            (TmV::Ob(ObTmV::Neu(n1, _)), TmV::Ob(ObTmV::Neu(n2, _))) => {
+            (TmV::Neu(n1, _), TmV::Neu(n2, _)) => {
                 if n1 == n2 {
                     Ok(())
                 } else {

packages/catlog/src/tt/modelgen.rs
@@ -60,13 +60,13 @@ impl Model {
     }
 
     /// Constructs an application of an object operation, if possible.
-    fn ob_app(&self, name: QualifiedName, ob_v: &ObTmV) -> Option<Ob> {
+    fn ob_app(&self, name: QualifiedName, tm_v: &TmV) -> Option<Ob> {
         match self {
             Model::Discrete(_) => None,
             Model::Modal(model) => {
                 let theory = model.theory();
                 let op = modal::ModalObOp::generator(name.clone());
-                let ob = self.make_ob(ob_v, &theory.ob_op_dom(&op).into());
+                let ob = self.make_ob(tm_v, &theory.ob_op_dom(&op).into())?;
                 let ob = ob.try_into().unwrap();
                 Some(Ob::Modal(modal::ModalOb::App(Box::new(ob), name)))
             }
@@ -80,7 +80,7 @@ impl Model {
             Model::Modal(_) => {
                 let ob_type: &modal::ModalObType = ob_type.try_into().unwrap();
                 let Some(modal::Modality::List(list_type)) = ob_type.modalities.last() else {
-                    panic!("Object type should be an application of a list modality");
+                    return None;
                 };
                 Some(Ob::Modal(modal::ModalOb::List(
                     *list_type,
@@ -113,18 +113,10 @@ impl Model {
         });
     }
 
-    /// Tries to make an object from a term.
-    fn maybe_make_ob(&self, val: &TmV, ob_type: &ObType) -> Option<Ob> {
+    /// Attempts to make an object from a term.
+    fn make_ob(&self, val: &TmV, ob_type: &ObType) -> Option<Ob> {
         match val {
-            TmV::Ob(ob) => Some(self.make_ob(ob, ob_type)),
-            _ => None,
-        }
-    }
-
-    /// Makes an object from an object term.
-    fn make_ob(&self, val: &ObTmV, ob_type: &ObType) -> Ob {
-        match val {
-            ObTmV::Neu(n, _) => {
+            TmV::Neu(n, _) => {
                 let mut segments = Vec::new();
                 let mut n = n.clone();
                 while let TmN_::Proj(n1, f, _) = &*n.clone() {
@@ -132,16 +124,16 @@ impl Model {
                     segments.push(*f);
                 }
                 segments.reverse();
-                self.ob_generator(segments.into())
+                Some(self.ob_generator(segments.into()))
             }
-            ObTmV::App(name, ob_v) => {
-                self.ob_app([*name].into(), ob_v).expect("theory should support operations")
-            }
-            ObTmV::List(elems) => {
+            TmV::App(name, tm_v) => self.ob_app([*name].into(), tm_v),
+            TmV::List(elems) => {
                 let el_type = ob_type.clone().list_arg().unwrap();
-                self.ob_list(elems.iter().map(|tm| self.make_ob(tm, &el_type)).collect(), ob_type)
-                    .expect("theory should support lists")
+                let elems: Option<Vec<_>> =
+                    elems.iter().map(|tm| self.make_ob(tm, &el_type)).collect();
+                self.ob_list(elems?, ob_type)
             }
+            _ => None,
         }
     }
 
@@ -217,8 +209,8 @@ impl<'a> ModelGenerator<'a> {
                 None
             }
             TyV_::Morphism(mt, dom, cod) => {
-                let dom = self.model.maybe_make_ob(dom, &self.theory.src_type(mt))?;
-                let cod = self.model.maybe_make_ob(cod, &self.theory.tgt_type(mt))?;
+                let dom = self.model.make_ob(dom, &self.theory.src_type(mt))?;
+                let cod = self.model.make_ob(cod, &self.theory.tgt_type(mt))?;
                 self.model.add_mor(prefix.into(), dom, cod, mt.clone());
                 None
             }

packages/catlog/src/tt/notebook_elab.rs
@@ -83,8 +83,8 @@ impl<'a> Elaborator<'a> {
             TmN::var(self.ctx.scope.len().into(), name, label),
             ty.clone().unwrap_or(TyV::unit()),
         );
-        let v = if let Some(ty) = &ty {
-            self.evaluator().eta(&v, ty)
+        let v = if ty.is_some() {
+            self.evaluator().eta(&v, ty.as_ref())
         } else {
             v
         };

packages/catlog/src/tt/text_elab.rs
@@ -191,7 +191,8 @@ impl TopElaborator {
                 }
                 let (_, tm_v, ty_v) = elab.syn(n);
                 let eval = elab.evaluator();
-                Some(TopElabResult::Output(format!("{}", eval.quote_tm(&eval.eta(&tm_v, &ty_v)),)))
+                let tm_s = eval.quote_tm(&eval.eta(&tm_v, Some(&ty_v)));
+                Some(TopElabResult::Output(format!("{tm_s}")))
             }
             "chk" => {
                 let theory = self.get_theory(tn.loc)?;
@@ -332,8 +333,8 @@ impl<'a> Elaborator<'a> {
             TmN::var(self.ctx.scope.len().into(), name, label),
             ty.clone().unwrap_or(TyV::unit()),
         );
-        let v = if let Some(ty) = &ty {
-            self.evaluator().eta(&v, ty)
+        let v = if ty.is_some() {
+            self.evaluator().eta(&v, ty.as_ref())
         } else {
             v
         };
@@ -578,7 +579,7 @@ impl<'a> Elaborator<'a> {
                 let dom = elab.theory().ob_op_dom(&ob_op);
                 let (arg_s, arg_v) = elab.chk(&TyV::object(dom), ob_n);
                 let cod = elab.theory().ob_op_cod(&ob_op);
-                (TmS::ob_app(name, arg_s), TmV::app(name, arg_v.unwrap_ob()), TyV::object(cod))
+                (TmS::ob_app(name, arg_s), TmV::app(name, arg_v), TyV::object(cod))
             }
             App2(L(_, Keyword("*")), f_n, g_n) => {
                 let (f_s, _, f_ty) = elab.syn(f_n);
@@ -692,7 +693,7 @@ impl<'a> Elaborator<'a> {
                     elab.loc = Some(ob_n.loc());
                     let (tm_s, tm_v) = elab.chk(&elem_ty_v, ob_n);
                     elem_stxs.push(tm_s);
-                    elem_vals.push(tm_v.unwrap_ob());
+                    elem_vals.push(tm_v);
                 }
                 (TmS::list(elem_stxs), TmV::list(elem_vals))
             }

packages/catlog/src/tt/val.rs
@@ -190,38 +190,6 @@ impl TmN {
     }
 }
 
-/// Values for terms of object type.
-///
-/// These are terms that can be potentially extracted to an object in a model.
-/// That is not possible for a generic [`TmV`]. This way of structuring values
-/// also ensures that it is impossible to construct, say, a list of records;
-/// only lists of objects are allowed.
-///
-/// TODO: Make `Rc`s uniform using inner enum.
-#[derive(Clone)]
-pub enum ObTmV {
-    /// Neutrals.
-    ///
-    /// We store the type because we need it for eta-expansion.
-    Neu(TmN, TyV),
-    /// Application of an object operation in the theory.
-    App(VarName, Rc<ObTmV>),
-    /// Lists of objects.
-    List(Rc<Vec<ObTmV>>),
-}
-
-impl ObTmV {
-    /// Smart constructor of applications of object operations.
-    pub fn app(name: VarName, x: ObTmV) -> Self {
-        ObTmV::App(name, Rc::new(x))
-    }
-
-    /// Smart constructor for lists.
-    pub fn list(elems: Vec<ObTmV>) -> Self {
-        ObTmV::List(Rc::new(elems))
-    }
-}
-
 /// Values for terms in the codiscrete mode.
 ///
 /// Note that this is *not* the value for a general term. So evaluating a `TmS`
@@ -232,8 +200,14 @@ impl ObTmV {
 /// don't need to bother making an inner type `TmV_`.
 #[derive(Clone)]
 pub enum TmV {
-    /// Object terms.
-    Ob(ObTmV),
+    /// Neutrals.
+    ///
+    /// We store the type because we need it for eta-expansion.
+    Neu(TmN, TyV),
+    /// Application of an object operation in the theory.
+    App(VarName, Rc<TmV>),
+    /// Lists of objects.
+    List(Rc<Vec<TmV>>),
     /// Records.
     Cons(Row<TmV>),
     /// The unique element of the unit type.
@@ -247,31 +221,23 @@ pub enum TmV {
 impl TmV {
     /// Smart constructor for neutrals.
     pub fn neu(n: TmN, ty: TyV) -> Self {
-        TmV::Ob(ObTmV::Neu(n, ty))
+        TmV::Neu(n, ty)
     }
 
     /// Smart constructor for applications of object operations.
-    pub fn app(name: VarName, x: ObTmV) -> Self {
-        TmV::Ob(ObTmV::app(name, x))
+    pub fn app(name: VarName, x: TmV) -> Self {
+        TmV::App(name, Rc::new(x))
     }
 
     /// Smart constructor for lists.
-    pub fn list(elems: Vec<ObTmV>) -> Self {
-        TmV::Ob(ObTmV::list(elems))
-    }
-
-    /// Unwraps an object term, or panics.
-    pub fn unwrap_ob(&self) -> ObTmV {
-        match self {
-            TmV::Ob(ob) => ob.clone(),
-            _ => panic!("expected term to be an object"),
-        }
+    pub fn list(elems: Vec<TmV>) -> Self {
+        TmV::List(Rc::new(elems))
     }
 
     /// Unwraps a neutral term, or panics.
     pub fn unwrap_neu(&self) -> TmN {
         match self {
-            TmV::Ob(ObTmV::Neu(n, _)) => n.clone(),
+            TmV::Neu(n, _) => n.clone(),
             _ => panic!("expected term to be a neutral"),
         }
     }

packages/catlog/examples/tt/text/test_modal_theories.dbltt
@@ -35,3 +35,9 @@ type SIRV := [
 ]
 
 generate SIRV
+
+#(should_fail)
+type BadOpApp := [
+   X : Object,
+   f : (Hom Object)[@tensor X, X],
+]

packages/catlog/examples/tt/text/test_modal_theories.dbltt.snapshot
@@ -66,3 +66,16 @@ generate SIRV
 #/ Unvax.recover : Unvax.I -> Unvax.R : Hom Object
 #/ vaccinate : Unvax.S -> V : Hom Object
 
+#(should_fail)
+type BadOpApp := [
+   X : Object,
+   f : (Hom Object)[@tensor X, X],
+]
+#/ declared: BadOpApp
+#/ expected errors:
+#/ error[elab]: synthesized type Object does not match expected type List.Symmetric Object:
+#/ object types Object and List.Symmetric Object are not equal
+#/ --> examples/tt/text/test_modal_theories.dbltt:42:29
+#/ 42|    f : (Hom Object)[@tensor X, X],
+#/ 42|                             ^
+