36c14d45af51103655b8418405bb6d046a53b562
Evan Patterson <evan@epatters.org>
2026-02-09 15:55:46 -0800

    REFACTOR: Allow custom renderer in `GraphVisualization` component. (#1027)
    This enables the stock-flow visualization to use the same pipeline as
    the other graph visualizations.

--- packages/frontend/src/visualization/graph_visualization.tsx
+++ packages/frontend/src/visualization/graph_visualization.tsx
@@ -1,8 +1,9 @@
 import type * as ELK from "elkjs";
-import { Match, Switch } from "solid-js";
+import { type Component, Match, Switch } from "solid-js";
 
 import { graphToElk } from "./elk";
 import { ElkSVG } from "./elk_svg";
+import type * as GraphLayout from "./graph_layout";
 import { type Config, elkOptions, graphvizOptions } from "./graph_layout_config";
 import type * as GraphSpec from "./graph_spec";
 import { type GraphvizAttributes, graphToViz } from "./graphviz";
@@ -17,6 +18,7 @@ layout engine, performs the layout, and renders the result as SVG.
 export function GraphVisualization(props: {
     graph: GraphSpec.Graph;
     config: Config;
+    renderer?: Component<{ graph: GraphLayout.Graph; ref?: SVGRefProp }>;
     ref?: SVGRefProp;
     elkLayoutOptions?: ELK.LayoutOptions;
     graphvizAttributes?: GraphvizAttributes;
@@ -31,12 +33,13 @@ export function GraphVisualization(props: {
     return (
         <Switch>
             <Match when={layout() === "elk"}>
-                <ElkSVG graph={elkGraph()} ref={props.ref} />
+                <ElkSVG graph={elkGraph()} renderer={props.renderer} ref={props.ref} />
             </Match>
             <Match when={layout() === "graphviz-directed" || layout() === "graphviz-undirected"}>
                 <GraphvizSVG
                     graph={graphToViz(props.graph, props.graphvizAttributes)}
                     options={graphvizOptions(props.config)}
+                    renderer={props.renderer}
                     ref={props.ref}
                 />
             </Match>
