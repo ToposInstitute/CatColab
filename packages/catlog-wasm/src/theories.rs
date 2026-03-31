5bb08b7b379a6a41be5cd7b8c2e8127947fcc066
Tim Hosgood <tim@topos.institute>
2026-03-31 21:11:25 +0100

    WIP: Polynomial ODE logic

--- packages/catlog-wasm/src/theories.rs
+++ packages/catlog-wasm/src/theories.rs
@@ -6,7 +6,7 @@
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 
-use catlog::dbl::theory::{self as theory, Unital};
+use catlog::dbl::theory::{self as theory, NonUnital, Unital};
 use catlog::one::Path;
 use catlog::stdlib::{analyses, models, theories, theory_morphisms};
 use catlog::zero::{QualifiedLabel, name};
@@ -427,6 +427,23 @@ impl ThSymMonoidalCategory {
     ) -> Result<bool, String> {
         let model = model.modal_unital().map_err(|_| "Model should be of a modal theory")?;
         Ok(analyses::reachability::subreachability(model, data))
+    }
+}
+
+/// A theory of systems of polynomial ODEs
+#[wasm_bindgen]
+pub struct ThSymMulticategory(Rc<theory::ModalDblTheory<NonUnital>>);
+
+#[wasm_bindgen]
+impl ThSymMulticategory {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self(Rc::new(theories::th_sym_multicategory()))
+    }
+
+    #[wasm_bindgen]
+    pub fn theory(&self) -> DblTheory {
+        DblTheory(self.0.clone().into())
     }
 }
 
