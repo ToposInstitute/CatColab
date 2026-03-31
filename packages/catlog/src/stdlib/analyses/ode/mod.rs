a401e0671c641aa4df9302babc4d51e73348e28f
Evan Patterson <evan@epatters.org>
2026-03-31 11:30:12 -0700

    CLEANUP: Eliminate extra copy of type alias `Parameter`.

--- packages/catlog/src/stdlib/analyses/ode/mod.rs
+++ packages/catlog/src/stdlib/analyses/ode/mod.rs
@@ -15,7 +15,7 @@ use tsify::Tsify;
 use crate::simulate::ode::{ODEProblem, ODESystem};
 use crate::zero::{QualifiedName, alg::Polynomial};
 
-/// Symbolic parameter in polynomial system.
+/// Symbolic parameter in a polynomial system.
 pub type Parameter<Id> = Polynomial<Id, f32, i8>;
 
 /// Solution to an ODE problem.
