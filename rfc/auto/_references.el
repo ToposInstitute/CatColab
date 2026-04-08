d11cb0885b8571562bbaa55206abbef7d28ddb3f
tslil-topos <tslil@topos.institute>
2026-04-08 12:47:49 +0100

    rfc draft: widening the definition of model, introducing the notion of instance

---
+++ rfc/auto/_references.el
@@ -0,0 +1,22 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "_references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "aduddell-et-al-2024"
+    "angiuli-gratzer-2025"
+    "baez-et-al-2023"
+    "cruttwell-shulman-2010"
+    "dreyer-2005"
+    "jacobs-1998"
+    "koudenburg-2020"
+    "lambert-patterson-2024"
+    "libkind-myers-2025"
+    "lynch-et-al-2024"
+    "pollack-2002"
+    "shulman-2016"
+    "hohc"
+    "clingman-2022"))
+ '(or :bibtex :latex))
+
--- rfc/filters.lua
+++ rfc/filters.lua
@@ -2,6 +2,7 @@ local system = require "pandoc.system"
 
 local rootdir = os.getenv "QUARTO_PROJECT_DIR"
 local cachedir = rootdir .. "/.svg-cache"
+local tikz_user_preamble = ""
 
 local thisfile = io.open(rootdir .. "/filters.lua")
 local thiscontent = thisfile:read "*all"
@@ -32,12 +33,31 @@ local tikz_templates = make_templates(tikz_template_paths)
 
 function trim(s) return (s:gsub("^%s*(.-)%s*$", "%1")) end
 
+local function meta_to_tex(v)
+  if v == nil then return "" end
+
+  local out = {}
+  local value_type = pandoc.utils.type(v)
+  if value_type == "Blocks" then
+    for _, b in ipairs(v) do
+      if b.t == "RawBlock" then
+        local format = b.format or (b.c and b.c[1])
+        local text = b.text or (b.c and b.c[2])
+        if format == "tex" and text ~= nil then table.insert(out, text) end
+      end
+    end
+  end
+
+  return table.concat(out, "\n")
+end
+
 local function tikz2image(template)
   return function(src, outfile)
     system.with_temporary_directory("tikz2image", function(tmpdir)
       system.with_working_directory(tmpdir, function()
         local f = io.open("tikz.tex", "w")
-        f:write(template[1] .. trim(src) .. template[2])
+        local before = template[1]:gsub("@OPTIONAL_PREAMBLE", tikz_user_preamble)
+        f:write(before .. trim(src) .. template[2])
         f:close()
         print()
         print "processing:"
@@ -75,14 +95,20 @@ local function memoize_svg(input, builder, key)
   return pandoc.Image({}, "_svgs/" .. fbasename)
 end
 
-function CodeBlock(el)
+local function handle_codeblock(el)
   local tikz_template = tikz_templates[el.classes[1]]
   if tikz_template ~= nil then
+    local before = tikz_template[1]:gsub("@OPTIONAL_PREAMBLE", tikz_user_preamble)
     return pandoc.Div(
-      memoize_svg(el.text, tikz2image(tikz_template), tikz_template[1] .. tikz_template[2]),
+      memoize_svg(el.text, tikz2image(tikz_template), before .. tikz_template[2]),
       { class = "tikz" }
     )
   else
     return el
   end
+end
+
+function Pandoc(doc)
+  tikz_user_preamble = meta_to_tex(doc.meta["tikz-preamble"])
+  return doc:walk({ CodeBlock = handle_codeblock })
 end
