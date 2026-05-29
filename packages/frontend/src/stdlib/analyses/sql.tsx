0cdadc29be2d1759688b17fd53d52f7fd72b97c3
Matt Cuffaro <cuffaro.matt@gmail.com>
2026-05-28 21:02:21 -0700

    ENH: Deferrable foreign keys for PostgresSQL backend (#1057)

--- packages/frontend/src/stdlib/analyses/sql.tsx
+++ packages/frontend/src/stdlib/analyses/sql.tsx
@@ -8,6 +8,8 @@ import { CodeView, BlockTitle, ErrorAlert, IconButton } from "catcolab-ui-compon
 import type { ModelAnalysisProps } from "../../analysis";
 import * as SQL from "./sql_types.ts";
 
+import styles from "./sql.module.css";
+
 const copyToClipboard = (text: string) => navigator.clipboard.writeText(text);
 
 const tooltip = () => (
@@ -31,14 +33,7 @@ const tooltip = () => (
 
 export function SQLHeader(sql: string) {
     return (
-        <div
-            style={{
-                display: "flex",
-                "align-items": "center",
-                "justify-content": "flex-end",
-                gap: "4px",
-            }}
-        >
+        <div class={styles.headerContainer}>
             <IconButton
                 onClick={() => copyToClipboard(sql)}
                 disabled={false}
@@ -112,10 +107,13 @@ export default function SQLSchemaAnalysis(
                             )}
                         </Match>
                         <Match when={result().tag === "Err"}>
-                            <ErrorAlert>
-                                <p>{"The model failed to compile into a SQL script."}</p>
-                                <p>{"Check for cycles in foreign key constraints."}</p>
-                            </ErrorAlert>
+                            <div>
+                                <BlockTitle title={props.title} settingsPane={BackendConfig()} />
+                                <ErrorAlert>
+                                    <p>{"The model failed to compile into a SQL script."}</p>
+                                    <p>{"Check for cycles in foreign key constraints."}</p>
+                                </ErrorAlert>
+                            </div>
                         </Match>
                     </Switch>
                 )}
