c95613e0b9e19c8a2ed63d4a5c88632c0db598db
Tim Hosgood <tim@topos.institute>
2026-07-08 17:19:51 +0100

    First suggestion of instances document type

--- packages/document-types/src/v2/instance.rs
+++ packages/document-types/src/v2/instance.rs
@@ -1,5 +1,32 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use tsify::Tsify;
+use uuid::Uuid;
 
-#[derive(PartialEq, Eq, Debug, Serialize, Deserialize, Tsify)]
-pub struct Table {}
+#[derive(PartialEq, Debug, Serialize, Deserialize, Tsify)]
+pub enum CellValue {
+    // If the column corresponds to an attribute morphism then we provide the value of the type.
+    Bool(bool),
+    Int(i32),
+    Float(f32),
+    String(String),
+    // If the column corresponds to a mapping morphism then we provide the uuid of the entity.
+    EntityRef(Uuid),
+}
+
+#[derive(PartialEq, Debug, Serialize, Deserialize, Tsify)]
+pub struct TableRow {
+    // The row "number".
+    id: Uuid,
+    // The content of the row, thought of as the predicate-object parts of a semantic triple, i.e. the
+    // column and the value.
+    content: HashMap<Uuid, CellValue>,
+}
+
+#[derive(PartialEq, Debug, Serialize, Deserialize, Tsify)]
+pub struct Table {
+    // The uuid of the entity to which this table corresponds.
+    entity: Uuid,
+    // The rows of the table.
+    rows: Vec<TableRow>,
+}
