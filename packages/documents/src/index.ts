9a34cabd79a8c43b6617f6d5f937616902f9c365
Kaspar Bumke <git@kaspar.systems>
2026-08-04 12:33:19 +0100

    REFACTOR: Rich text in own file

--- packages/documents/src/index.ts
+++ packages/documents/src/index.ts
@@ -1,9 +1,10 @@
 export { createBinder } from "./binder";
 export type { Binder } from "./binder";
-export type { MorphismCell, ObjectCell, RichTextCell } from "./cell";
+export type { MorphismCell, ObjectCell } from "./cell";
 export type { ModelDocument } from "./model-document";
 export type { NotebookDocument } from "./notebook-document";
 export type { Notebook } from "./notebook";
 export { notebookFromModel } from "./notebook";
+export type { RichTextCell } from "./rich-text";
 export { defineMorphism, defineObject, defineShape, RichText } from "./shape";
 export type { MorphismEndpoint, MorphismEndpoints, MorphismType, ObjectType, Shape } from "./shape";
