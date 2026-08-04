dac8f85192c49e013b541f397c65d5226d63424a
tslil-topos <tslil@topos.institute>
2026-08-04 10:56:46 +0100

    infrastructure: tslil ssh key

--- infrastructure/ssh-keys.nix
+++ infrastructure/ssh-keys.nix
@@ -1,5 +1,6 @@
 let
   allUserKeys = {
+    tslil = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvWJTuBjKsZTDpxlmIB432YqrqhxrTwutKHABlPGvt/ tslil@a6b530d2";
     owen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2sBTuqGoEXRWpBRqTBwZZPDdLGGJ0GQcuX5dfIZKb4 o@red-special";
     epatters = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKXx6wMJSeYKCHNmbyR803RQ72uto9uYsHhAPPWNl2D evan@epatters.org";
     jmoggr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMiaHaeJ5PQL0mka/lY1yGXIs/bDK85uY1O3mLySnwHd j@jmoggr.com";
