d14012a600a0d4f27a428c3a6fcd9bbddc4d0763
tslil-topos <tslil@topos.institute>
2026-08-13 15:52:29 +0100

    OBEE-31: add simple-schema to logics
    It's simple (schema).

---
+++ packages/logics/src/simple-schema.ts
@@ -0,0 +1,17 @@
+import { defineMorphism, defineObject, defineShape, RichText } from "catcolab-documents";
+
+export const Entity = defineObject({ tag: "Basic", content: "Entity" });
+export const AttrType = defineObject({ tag: "Basic", content: "AttrType" });
+
+export const Mapping = defineMorphism({ tag: "Hom", content: Entity.obType });
+export const Attr = defineMorphism(
+    { tag: "Basic", content: "Attr" },
+    { domain: Entity.obType, codomain: AttrType.obType },
+);
+
+export const SimpleSchema = defineShape({
+    theory: "simple-schema",
+    objects: [Entity, AttrType],
+    morphisms: [Mapping, Attr],
+    informal: [RichText],
+});
